6cff3927b0785f14a8512fc2a5ed08a8829417b4
Simon Strandgaard <neoneye@gmail.com>
2024-07-15 16:20:31 +0200

    generate_one_histogram_dataset_item added

--- generate_dataset_histogram.py
+++ generate_dataset_histogram.py
@@ -3,30 +3,71 @@ import os
 import random
 from simon_arc_lab.histogram import *
 
+name_formats = [
+    'SIMONARCHISTOGRAM',
+    'SIMON-ARC-HISTOGRAM',
+    'SIMONS-ARC-HISTOGRAM',
+    'SIMONS-HISTOGRAM',
+    'Simon-ARC-Histogram',
+    'SimonsHistogram',
+    'Simons-Histogram',
+    'simons-Arc-Histogram',
+    'simon-histogram',
+    'simonhistogram',
+    'simonshistogram',
+]
+
+def generate_one_histogram_dataset_item(seed):
+    transformation_ids = [
+        'number_of_unique_colors',
+    ]
+    transformation_weights = [20]
+    transformation_id = random.Random(seed + 1001).choices(transformation_ids, weights=transformation_weights, k=1)[0]
+
+    name_format = random.Random(seed + 1005).choice(name_formats)
+
+    instructions_number_of_unique_colors = [
+        f'{name_format}, number of unique colors',
+        f'number of unique colors in {name_format}',
+        f'how many unique colors are there in {name_format}',
+        f'{name_format}, unique color count',
+    ]
+
+    instructions = None
+    if transformation_id == 'number_of_unique_colors':
+        instructions = instructions_number_of_unique_colors
+    else:
+        raise Exception("Unreachable code reached")
+
+    instruction = random.Random(seed + 1006).choice(instructions)
+
+    histogram = Histogram.create_random(seed + 1007, 1, 9, 1, 20)
+
+    input = histogram.pretty()
+
+    output = None
+    if transformation_id == 'number_of_unique_colors':
+        output = str(histogram.number_of_unique_colors())
+    else:
+        raise Exception("Unreachable code reached")
+
+    dict = {
+        'instruction': instruction,
+        'input': input,
+        'output': output
+    }
+    return dict
+
 def generate_two_histogram_dataset_item(seed):
     transformation_ids = [
         'add', 
         'subtract',
         'max',
         'min',
-        # 'number_of_unique_colors',
     ]
     transformation_weights = [20, 20, 20, 20]
     transformation_id = random.Random(seed + 1001).choices(transformation_ids, weights=transformation_weights, k=1)[0]
 
-    name_formats = [
-        'SIMONARCHISTOGRAM',
-        'SIMON-ARC-HISTOGRAM',
-        'SIMONS-ARC-HISTOGRAM',
-        'SIMONS-HISTOGRAM',
-        'Simon-ARC-Histogram',
-        'SimonsHistogram',
-        'Simons-Histogram',
-        'simons-Arc-Histogram',
-        'simon-histogram',
-        'simonhistogram',
-        'simonshistogram',
-    ]
     name_format = random.Random(seed + 1005).choice(name_formats)
 
     instructions_add = [
@@ -67,13 +108,6 @@ def generate_two_histogram_dataset_item(seed):
         f'Maximum of {name_format}',
     ]
 
-    instructions_number_of_unique_colors = [
-        f'{name_format}, number of unique colors',
-        f'number of unique colors in {name_format}',
-        f'how many unique colors are there in {name_format}',
-        f'{name_format}, unique color count',
-    ]
-
     instructions = None
     if transformation_id == 'add':
         instructions = instructions_add
@@ -83,8 +117,6 @@ def generate_two_histogram_dataset_item(seed):
         instructions = instructions_min
     elif transformation_id == 'max':
         instructions = instructions_max
-    elif transformation_id == 'number_of_unique_colors':
-        instructions = instructions_number_of_unique_colors
     else:
         raise Exception("Unreachable code reached")
 
@@ -118,7 +150,10 @@ def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=2
     dataset = []
     dataset_byte_size = 0
     for i in range(max_num_samples):
-        item = generate_two_histogram_dataset_item(seed_start + i)
+        if i % 10 == 0:
+            item = generate_one_histogram_dataset_item(seed_start + i)
+        else:
+            item = generate_two_histogram_dataset_item(seed_start + i)
         bytes = len(json.dumps(item))
         if dataset_byte_size + bytes > max_byte_size:
             break
