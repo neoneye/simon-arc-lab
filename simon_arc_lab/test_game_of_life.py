3834bb2f7d6eb6a0297de4bf827eb403fb85fbc1
Simon Strandgaard <neoneye@gmail.com>
2024-07-17 16:47:55 +0200

    nicer test of GoL

--- simon_arc_lab/test_game_of_life.py
+++ simon_arc_lab/test_game_of_life.py
@@ -4,31 +4,35 @@ from .game_of_life import *
 
 class TestGameOfLife(unittest.TestCase):
     def test_blinker(self):
-        input = np.zeros((5, 5), dtype=np.uint8)
-        input[1, 2] = 1
-        input[2, 2] = 1
-        input[3, 2] = 1
+        input = np.array([
+            [0, 0, 0, 0, 0], 
+            [0, 0, 1, 0, 0], 
+            [0, 0, 1, 0, 0], 
+            [0, 0, 1, 0, 0], 
+            [0, 0, 0, 0, 0]], dtype=np.uint8)
         actual = game_of_life_wrap(input)
-        expected = np.zeros((5, 5), dtype=np.uint8)
-        expected[2, 1] = 1
-        expected[2, 2] = 1
-        expected[2, 3] = 1
+        expected = np.array([
+            [0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0], 
+            [0, 1, 1, 1, 0], 
+            [0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0]], dtype=np.uint8)
         self.assertTrue(np.array_equal(actual, expected))
 
     def test_glider(self):
-        input = np.zeros((5, 5), dtype=np.uint8)
-        input[1, 3] = 1
-        input[2, 1] = 1
-        input[2, 3] = 1
-        input[3, 2] = 1
-        input[3, 3] = 1
+        input = np.array([
+            [0, 0, 0, 0, 0], 
+            [0, 0, 0, 1, 0], 
+            [0, 1, 0, 1, 0], 
+            [0, 0, 1, 1, 0], 
+            [0, 0, 0, 0, 0]], dtype=np.uint8)
         actual = game_of_life_wrap(input)
-        expected = np.zeros((5, 5), dtype=np.uint8)
-        expected[1, 2] = 1
-        expected[2, 3] = 1
-        expected[2, 4] = 1
-        expected[3, 2] = 1
-        expected[3, 3] = 1
+        expected = np.array([
+            [0, 0, 0, 0, 0], 
+            [0, 0, 1, 0, 0], 
+            [0, 0, 0, 1, 1], 
+            [0, 0, 1, 1, 0], 
+            [0, 0, 0, 0, 0]], dtype=np.uint8)
         self.assertTrue(np.array_equal(actual, expected))
 
 if __name__ == '__main__':
