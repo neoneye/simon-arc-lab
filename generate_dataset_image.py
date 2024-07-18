3e44cf0f4a17c824c9eb6d14c7abe0f36a48dea5
Simon Strandgaard <neoneye@gmail.com>
2024-07-18 17:55:57 +0200

    Dataset with slightly bigger image sizes

--- generate_dataset_image.py
+++ generate_dataset_image.py
@@ -143,8 +143,8 @@ def generate_deserialize_dataset_item(seed):
     :param seed: The seed for the random number generator
     :return: A dictionary with the instruction, input, and output
     """
-    min_image_size = 5
-    max_image_size = 20
+    min_image_size = 10
+    max_image_size = 30
 
     instruction_ids = [
         'pixels', 
@@ -455,7 +455,7 @@ def generate_deserialize_dataset_item(seed):
     }
     return dict
 
-def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=500000):
+def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=600000):
     dataset = []
     dataset_byte_size = 0
     for i in range(max_num_samples):
