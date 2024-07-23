951331ace148ca2fba1bc67c1330e4908caaaa23
Simon Strandgaard <neoneye@gmail.com>
2024-07-23 22:23:16 +0200

    exercise with bigger images

--- generate_dataset_cellular_automaton.py
+++ generate_dataset_cellular_automaton.py
@@ -17,8 +17,8 @@ def generate_dataset_item(seed):
     :param seed: The seed for the random number generator
     :return: A dictionary with the instruction, input, and output
     """
-    min_image_size = 5
-    max_image_size = 10
+    min_image_size = 6
+    max_image_size = 12
 
     transformation_ids = [
         'gameoflife_wrap',
@@ -264,7 +264,7 @@ def generate_dataset_item(seed):
     }
     return result_dict
 
-def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=500000):
+def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=600000):
     dataset = []
     dataset_byte_size = 0
     for i in range(max_num_samples):
