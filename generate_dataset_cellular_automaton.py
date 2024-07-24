69be13f3bc337c88eb4ef656c4ea75bad14e90ca
Simon Strandgaard <neoneye@gmail.com>
2024-07-24 20:19:01 +0200

    exercise with bigger images

--- generate_dataset_cellular_automaton.py
+++ generate_dataset_cellular_automaton.py
@@ -272,8 +272,8 @@ def generate_dataset_item_transform_recognize(seed):
     :param seed: The seed for the random number generator
     :return: A dictionary with the instruction, input, and output
     """
-    min_image_size = 4
-    max_image_size = 8
+    min_image_size = 6
+    max_image_size = 10
 
     transformation_id = 'recognize_transformation'
 
@@ -416,7 +416,7 @@ def generate_dataset_item_transform_recognize(seed):
     }
     return result_dict
 
-def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=1100000):
+def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=1200000):
     dataset = []
     dataset_byte_size = 0
     for i in range(max_num_samples):
