6ec1c51b2c62f63b2703e70dbbc65d109f2512a4
Simon Strandgaard <neoneye@gmail.com>
2024-07-25 15:05:58 +0200

    ImageShape3x3 added

---
+++ simon_arc_lab/image_shape3x3.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+class ImageShape3x3:
+    TOP_LEFT = 1
+    TOP = 2
+    TOP_RIGHT = 4
+    LEFT = 8
+    RIGHT = 16
+    BOTTOM_LEFT = 32
+    BOTTOM = 64
+    BOTTOM_RIGHT = 128
+
+    @classmethod
+    def apply(cls, image: np.array) -> np.array:
+        """
+        Identify the shape of the 3x3 neighborhood of each pixel in the image.
+
+        The shape is represented as an 8-bit integer, where each bit corresponds to a neighbor.
+        The bit is set to 1 if the neighbor has the same color as the center pixel.
+
+        When the entire mask is zero, it means no neighbors have the same color as the center pixel.
+
+        :param image: image
+        return: image with the same size as the input image
+        """
+
+        height, width = image.shape
+        new_image = np.zeros((height, width), dtype=np.uint8)
+        
+        for y in range(height):
+            for x in range(width):
+                center_color = image[y, x]
+                same_color_mask = 0
+                
+                # Check all 8 neighbors, in total 8 bits, are they the same color as the center pixel
+                current_bit = 0
+                for dy in [-1, 0, 1]:
+                    for dx in [-1, 0, 1]:
+                        if dx == 0 and dy == 0:
+                            continue
+                        ny, nx = y + dy, x + dx
+                        if 0 <= ny < height and 0 <= nx < width:
+                            if image[ny, nx] == center_color:
+                                same_color_mask |= 1 << current_bit
+                        current_bit += 1
+                
+                new_image[y, x] = same_color_mask
+        
+        return new_image
---
+++ simon_arc_lab/test_image_shape3x3.py
@@ -0,0 +1,95 @@
+import unittest
+import numpy as np
+from .image_shape3x3 import *
+
+class TestImageShape3x3(unittest.TestCase):
+    def test_no_neighbors_with_same_color(self):
+        image = np.array([
+            [1, 2, 3, 4], 
+            [5, 6, 7, 8],
+            [9, 0, 1, 2]], dtype=np.uint8)
+        actual = ImageShape3x3.apply(image)
+        expected = np.array([
+            [0, 0, 0, 0], 
+            [0, 0, 0, 0], 
+            [0, 0, 0, 0]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_vertical_lines(self):
+        image = np.array([
+            [1, 2, 3, 4], 
+            [1, 2, 3, 4], 
+            [1, 2, 3, 4]], dtype=np.uint8)
+        actual = ImageShape3x3.apply(image)
+        a = ImageShape3x3.BOTTOM # same color below
+        b = ImageShape3x3.TOP | ImageShape3x3.BOTTOM # same color above and below
+        c = ImageShape3x3.TOP # same color above
+        self.assertEqual(b, 0x42)
+        expected = np.array([
+            [a, a, a, a], 
+            [b, b, b, b], 
+            [c, c, c, c]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_horizontal_lines(self):
+        image = np.array([
+            [1, 1, 1, 1], 
+            [2, 2, 2, 2], 
+            [3, 3, 3, 3]], dtype=np.uint8)
+        actual = ImageShape3x3.apply(image)
+        a = ImageShape3x3.RIGHT # same color to the right
+        b = ImageShape3x3.RIGHT | ImageShape3x3.LEFT # same color to the right and left
+        c = ImageShape3x3.LEFT # same color to the left
+        self.assertEqual(b, 0x18)
+        expected = np.array([
+            [a, b, b, c], 
+            [a, b, b, c], 
+            [a, b, b, c]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_diagonal_lines_topleft_bottomright(self):
+        image = np.array([
+            [1, 3, 2, 1], 
+            [2, 1, 3, 2], 
+            [3, 2, 1, 3]], dtype=np.uint8)
+        actual = ImageShape3x3.apply(image)
+        a = ImageShape3x3.BOTTOM_RIGHT
+        b = ImageShape3x3.TOP_LEFT | ImageShape3x3.BOTTOM_RIGHT
+        c = ImageShape3x3.TOP_LEFT
+        self.assertEqual(b, 0x81)
+        expected = np.array([
+            [a, a, a, 0], 
+            [a, b, b, c], 
+            [0, c, c, c]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_diagonal_lines_topright_bottomleft(self):
+        image = np.array([
+            [3, 2, 1, 3],
+            [2, 1, 3, 2], 
+            [1, 3, 2, 1]], dtype=np.uint8)
+        actual = ImageShape3x3.apply(image)
+        a = ImageShape3x3.BOTTOM_LEFT
+        b = ImageShape3x3.TOP_RIGHT | ImageShape3x3.BOTTOM_LEFT
+        c = ImageShape3x3.TOP_RIGHT
+        self.assertEqual(b, 0x24)
+        expected = np.array([
+            [0, a, a, a],
+            [c, b, b, a], 
+            [c, c, c, 0]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_all_same_color(self):
+        image = np.array([
+            [9, 9, 9, 9], 
+            [9, 9, 9, 9], 
+            [9, 9, 9, 9]], dtype=np.uint8)
+        actual = ImageShape3x3.apply(image)
+        expected = np.array([
+            [0xd0, 0xf8, 0xf8, 0x68], 
+            [0xd6, 0xff, 0xff, 0x6b], 
+            [0x16, 0x1f, 0x1f, 0x0b]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+if __name__ == '__main__':
+    unittest.main()
