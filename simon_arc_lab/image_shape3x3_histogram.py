0c4100edb9f199b5d3a0b76f6cfe28c4ae48f940
Simon Strandgaard <neoneye@gmail.com>
2024-07-26 14:55:51 +0200

    Histogram.number_of_unique_colors_in_diamond5() added

--- simon_arc_lab/image_shape3x3_histogram.py
+++ simon_arc_lab/image_shape3x3_histogram.py
@@ -4,7 +4,7 @@ from .histogram import Histogram
 class ImageShape3x3Histogram:
 
     @classmethod
-    def number_of_unique_colors(cls, image: np.array) -> np.array:
+    def number_of_unique_colors_all9(cls, image: np.array) -> np.array:
         """
         Count the number of unique colors inside a 3x3 neighborhood, all 9 pixels.
 
@@ -30,6 +30,40 @@ class ImageShape3x3Histogram:
                         ny, nx = y + dy, x + dx
                         if 0 <= ny < height and 0 <= nx < width:
                             histogram.increment(image[ny, nx])
+                
+                new_image[y, x] = histogram.number_of_unique_colors()
+        
+        return new_image
+
+    @classmethod
+    def number_of_unique_colors_in_diamond5(cls, image: np.array) -> np.array:
+        """
+        Count the number of unique colors inside a 3x3 neighborhood, except the corners, so 5 pixels.
+
+        When all the 5 pixels use different colors, the number of unique colors is 5.
+
+        When all the 5 pixels use the same color, the number of unique colors is 1.
+
+        Pixes that are outside the image are not counted.
+
+        :param image: image
+        return: image with the same size as the input image
+        """
+
+        height, width = image.shape
+        new_image = np.zeros((height, width), dtype=np.uint8)
+        
+        for y in range(height):
+            for x in range(width):
+                dx_dy_list = [(0, -1), (-1, 0), (0, 0), (1, 0), (0, 1)]
+                histogram = Histogram.empty()
+                
+                for dx_dy in dx_dy_list:
+                    dx = dx_dy[0]
+                    dy = dx_dy[1]
+                    ny, nx = y + dy, x + dx
+                    if 0 <= ny < height and 0 <= nx < width:
+                        histogram.increment(image[ny, nx])
                 
                 new_image[y, x] = histogram.number_of_unique_colors()
         
--- simon_arc_lab/test_image_shape3x3_histogram.py
+++ simon_arc_lab/test_image_shape3x3_histogram.py
@@ -3,40 +3,88 @@ import numpy as np
 from .image_shape3x3_histogram import *
 
 class TestImageShape3x3Histogram(unittest.TestCase):
-    def test_all_unique_colors(self):
+    def test_all9_unique_colors(self):
         image = np.array([
             [1, 2, 3, 1], 
             [4, 5, 6, 4],
             [7, 8, 9, 7]], dtype=np.uint8)
-        actual = ImageShape3x3Histogram.number_of_unique_colors(image)
+        actual = ImageShape3x3Histogram.number_of_unique_colors_all9(image)
         expected = np.array([
             [4, 6, 6, 4], 
             [6, 9, 9, 6], 
             [4, 6, 6, 4]], dtype=np.uint8)
         self.assertTrue(np.array_equal(actual, expected))
 
-    def test_same_color(self):
+    def test_all9_same_color(self):
         image = np.array([
             [5, 5, 5, 5], 
             [5, 5, 5, 5], 
             [5, 5, 5, 5]], dtype=np.uint8)
-        actual = ImageShape3x3Histogram.number_of_unique_colors(image)
+        actual = ImageShape3x3Histogram.number_of_unique_colors_all9(image)
         expected = np.array([
             [1, 1, 1, 1], 
             [1, 1, 1, 1], 
             [1, 1, 1, 1]], dtype=np.uint8)
         self.assertTrue(np.array_equal(actual, expected))
 
-    def test_two_colors(self):
+    def test_all9_two_colors(self):
         image = np.array([
             [6, 6, 5, 5], 
             [6, 6, 5, 5], 
             [6, 6, 5, 5]], dtype=np.uint8)
-        actual = ImageShape3x3Histogram.number_of_unique_colors(image)
+        actual = ImageShape3x3Histogram.number_of_unique_colors_all9(image)
         expected = np.array([
             [1, 2, 2, 1], 
             [1, 2, 2, 1], 
             [1, 2, 2, 1]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_diamond5_unique_colors(self):
+        image = np.array([
+            [1, 2, 3, 1], 
+            [4, 5, 6, 4],
+            [7, 8, 9, 7]], dtype=np.uint8)
+        actual = ImageShape3x3Histogram.number_of_unique_colors_in_diamond5(image)
+        expected = np.array([
+            [3, 4, 4, 3], 
+            [4, 5, 5, 4], 
+            [3, 4, 4, 3]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_diamond5_same_color(self):
+        image = np.array([
+            [5, 5, 5, 5], 
+            [5, 5, 5, 5], 
+            [5, 5, 5, 5]], dtype=np.uint8)
+        actual = ImageShape3x3Histogram.number_of_unique_colors_in_diamond5(image)
+        expected = np.array([
+            [1, 1, 1, 1], 
+            [1, 1, 1, 1], 
+            [1, 1, 1, 1]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_diamond5_two_colors(self):
+        image = np.array([
+            [6, 6, 5, 5], 
+            [6, 6, 5, 5], 
+            [6, 6, 5, 5]], dtype=np.uint8)
+        actual = ImageShape3x3Histogram.number_of_unique_colors_in_diamond5(image)
+        expected = np.array([
+            [1, 2, 2, 1], 
+            [1, 2, 2, 1], 
+            [1, 2, 2, 1]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_diamond5_a_diamond_of_one_color(self):
+        image = np.array([
+            [5, 5, 6, 5, 5], 
+            [5, 6, 6, 6, 5], 
+            [5, 5, 6, 5, 5]], dtype=np.uint8)
+        actual = ImageShape3x3Histogram.number_of_unique_colors_in_diamond5(image)
+        expected = np.array([
+            [1, 2, 2, 2, 1], 
+            [2, 2, 1, 2, 2], 
+            [1, 2, 2, 2, 1]], dtype=np.uint8)
         self.assertTrue(np.array_equal(actual, expected))
 
 if __name__ == '__main__':
