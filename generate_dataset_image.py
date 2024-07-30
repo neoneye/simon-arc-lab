a8834353b9d585d3f0f0cb0430dc6caeb4284b94
Simon Strandgaard <neoneye@gmail.com>
2024-07-30 20:33:44 +0200

    rotate cw/ccw with bigger images

--- generate_dataset_image.py
+++ generate_dataset_image.py
@@ -169,13 +169,14 @@ def generate_deserialize_dataset_item(seed_start, item_index):
     :return: A dictionary with the instruction, input, and output
     """
     min_image_size = 1
-    max_image_size = 15
+    max_image_size = 22
 
     seed = seed_start + item_index
     random.seed(seed)
 
     # image_seed = seed + 1006 
-    image_seed = seed_start + (item_index // 2)  # Use the same image for rotate_cw and rotate_ccw
+    # image_seed = seed_start + (item_index // 2)  # Use the same image for rotate_cw and rotate_ccw
+    image_seed = seed_start + (item_index // 3)  # Use the same image for rotate_cw and rotate_ccw and get_column_as_list
     rle_string, image = generate_rle_string(
         seed=image_seed, 
         min_image_size=min_image_size, 
@@ -210,10 +211,12 @@ def generate_deserialize_dataset_item(seed_start, item_index):
     # transformation_weights = [0, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
     # transformation_weights = [0, 0, 0, 0, 0, 0, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     transformation_weights = [0, 0, 0, 0, 0, 0, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10]
-    transformation_id = random.choices(transformation_ids, weights=transformation_weights, k=1)[0]
-    # transformation_id = 'rotate_cw'
-    # if item_index % 2 == 0:
-    #     transformation_id = 'rotate_ccw'
+    # transformation_id = random.choices(transformation_ids, weights=transformation_weights, k=1)[0]
+    transformation_id = 'rotate_cw'
+    if item_index % 3 == 0:
+        transformation_id = 'rotate_ccw'
+    if item_index % 3 == 1:
+        transformation_id = 'get_column_as_list'
 
     names_pixels = [
         'Pixels',
@@ -598,7 +601,7 @@ def generate_deserialize_dataset_item(seed_start, item_index):
     }
     return result_dict
 
-def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=4300000):
+def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=4500000):
     dataset = []
     dataset_byte_size = 0
     for i in range(max_num_samples):
