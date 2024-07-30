782e5e8316a5622206e0c59e3e3cddde4c62d043
Simon Strandgaard <neoneye@gmail.com>
2024-07-30 16:19:46 +0200

    smaller images

--- generate_dataset_image.py
+++ generate_dataset_image.py
@@ -169,7 +169,7 @@ def generate_deserialize_dataset_item(seed_start, item_index):
     :return: A dictionary with the instruction, input, and output
     """
     min_image_size = 1
-    max_image_size = 21
+    max_image_size = 5
 
     seed = seed_start + item_index
     random.seed(seed)
@@ -598,7 +598,7 @@ def generate_deserialize_dataset_item(seed_start, item_index):
     }
     return result_dict
 
-def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=4000000):
+def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=4100000):
     dataset = []
     dataset_byte_size = 0
     for i in range(max_num_samples):
