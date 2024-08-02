f69a52e05cf5950bd3e6e333d5f0fb76d4f60003
Simon Strandgaard <neoneye@gmail.com>
2024-08-02 17:26:15 +0200

    Predict the number of rows

--- run_arcagi.py
+++ run_arcagi.py
@@ -3,7 +3,7 @@ from simon_arc_lab.task import *
 from simon_arc_lab.task_formatter_rle_compact import *
 from model.runner import *
 
-model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model122'
+model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model127'
 
 
 filename = 'testdata/25ff71a9.json'
@@ -20,16 +20,34 @@ output_ids = task_formatter.output_ids()
 dataset_name = 'SIMONSOLVETRANSLATE'
 
 dataset_items = []
+# Predict the height of the test output image
 for test_index in range(task.count_tests):
-    # print(f"Test {test_index}")
-
     input = task_formatter.to_string()
-
     expected_output = task.test_output(test_index)
-
     test_output_id = output_ids[task_without_test_output.count_examples + test_index]
 
-    # TODO: let the model make a guess about the output_height
+    output_height = expected_output.shape[0]
+    output = str(output_height)
+
+    instruction = f"{dataset_name} {test_output_id} predict height"
+    benchmark_id = f'dataset={filename} predict=height test_index={test_index}'
+
+    dataset_item = {
+        'instruction': instruction,
+        'input': input,
+        'output': output,
+        'benchmark': benchmark_id
+    }
+    # print(dataset_item)
+    dataset_items.append(dataset_item)
+
+# Predict the pixels of the test output image
+for test_index in range(task.count_tests):
+    input = task_formatter.to_string()
+    expected_output = task.test_output(test_index)
+    test_output_id = output_ids[task_without_test_output.count_examples + test_index]
+
+    # TODO: let the model make a guess about the output_height, and use the height here
     output_height = 3
     for output_y in range(output_height):
         instruction = f"{dataset_name}, {test_output_id}, predict row {output_y}"
@@ -37,7 +55,7 @@ for test_index in range(task.count_tests):
         pixel_list = image_get_row_as_list(expected_output, output_y)
 
         output = ''.join(map(str, pixel_list))
-        benchmark_id = f'dataset={filename} test_index={test_index} output_y={output_y}'
+        benchmark_id = f'dataset={filename} predict=pixels test_index={test_index} output_y={output_y}'
 
         dataset_item = {
             'instruction': instruction,
