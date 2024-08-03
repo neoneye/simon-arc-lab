55b1cb4f3ad27fef185d641cdbeb7bff1f5d449e
Simon Strandgaard <neoneye@gmail.com>
2024-08-03 12:10:35 +0200

    generated dataset

--- dataset_solve_color.py
+++ dataset_solve_color.py
@@ -32,8 +32,8 @@ def generate_task_swap_colors(seed: int) -> Task:
     random.seed(seed)
 
     count_example = random.randint(2, 3)
-    # count_test = random.randint(1, 2)
-    count_test = 1
+    count_test = random.randint(1, 2)
+    # count_test = 1
     task = Task()
     min_width = 1
     max_width = 5
@@ -227,7 +227,7 @@ def generate_dataset_item_list(seed: int) -> list[dict]:
 
     return all_dataset_items
 
-def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=100000):
+def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=200000):
     dataset = []
     dataset_byte_size = 0
     stop = False
