c0a57d6866fb3847de08d2a436b1bb54657b9d46
Simon Strandgaard <neoneye@gmail.com>
2024-08-04 21:24:01 +0200

    Visualization of datasets

---
+++ dataset/plot.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from collections import Counter
+
+def plot_prompt_length_distribution(dataset_items: list[dict]):
+    counter = Counter()
+    for item in dataset_items:
+        instruction = item['instruction']
+        input = item['input']
+        length = len(instruction) + len(input) + 1
+        counter[length] += 1
+
+    instructions = list(counter.keys())
+    counts = list(counter.values())
+
+    plt.bar(instructions, counts)
+    plt.xticks(rotation=90)
+    plt.title('Prompt length distribution')
+    plt.show()
+
+def plot_response_length_distribution(dataset_items: list[dict]):
+    counter = Counter()
+    for item in dataset_items:
+        output = item['output']
+        length = len(output)
+        counter[length] += 1
+
+    instructions = list(counter.keys())
+    counts = list(counter.values())
+
+    plt.bar(instructions, counts)
+    plt.xticks(rotation=90)
+    plt.title('Response length distribution')
+    plt.show()
