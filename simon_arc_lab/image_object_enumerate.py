5f3d76db18450f5d382ce9e267abe81d60d9ac70
Simon Strandgaard <neoneye@gmail.com>
2024-08-05 23:21:46 +0200

    documentation

--- simon_arc_lab/image_object_enumerate.py
+++ simon_arc_lab/image_object_enumerate.py
@@ -12,18 +12,18 @@ def object_enumerate(objects: list[np.array]) -> np.array:
     
     The pixels for the `Nth` object is assigned `value N-1`.
     
-    There can minimum be 1 object. If zero objects are provided then an `Err` is returned.
+    There can minimum be 1 object. If zero objects are provided then an exception is raised.
     
-    There can maximum be 255 objects. If more objects are provided then an `Err` is returned.
+    There can maximum be 255 objects. If more objects are provided then an exception is raised.
     
     The objects doesn't have to cover the entire area. The areas not covered by any object is assigned the `value 0`.
     
     Each object is a mask, where it's 1 the object is present, where it's 0 there is no object.
-    If the object mask contains values that isn't 0 or 1, then an `Err` is returned.
+    If the object mask contains values that isn't 0 or 1, then an exception is raised.
     
-    The objects are supposed to "not overlap" with each other. If they do overlap then an `Err` is returned.
+    The objects are supposed to "not overlap" with each other. If they do overlap then an exception is raised.
     
-    All the objects are supposed to have the same `width x height`, otherwise an `Err` is returned.
+    All the objects are supposed to have the same `width x height`, otherwise an exception is raised.
     The size of the output image is `width x height`.
     """
 
