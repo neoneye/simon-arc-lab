d72df8d81febef5f7bbf9423c7c773cfa73ba86d
Simon Strandgaard <neoneye@gmail.com>
2024-08-05 18:26:04 +0200

    generated new combined dataset

--- simon_arc_lab/pixel_connectivity.py
+++ simon_arc_lab/pixel_connectivity.py
@@ -1,5 +1,12 @@
 from enum import Enum
 
 class PixelConnectivity(Enum):
+    # Considers only the 4 neighbors around the center pixel, the top/bottom/left/right pixels.
+    #
+    # Don't visit the corners.
     CONNECTIVITY4 = 4
+
+    # Considers all the 8 neighbors around the center pixel.
+    #
+    # This can be useful for diagonal flood filling via corners.
     CONNECTIVITY8 = 8
