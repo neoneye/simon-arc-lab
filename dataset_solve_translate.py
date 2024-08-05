a3756867eb012481e681a3d96a94956ad4b66d6a
Simon Strandgaard <neoneye@gmail.com>
2024-08-05 09:41:50 +0200

    generated new combined dataset

--- dataset_solve_translate.py
+++ dataset_solve_translate.py
@@ -188,7 +188,7 @@ def generate_dataset_item_list(seed: int) -> list[dict]:
 
     return all_dataset_items
 
-def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=1700000):
+def generate_dataset(max_num_samples=1000, max_byte_size=1024*1024, seed_start=1800000):
     dataset = []
     dataset_byte_size = 0
     stop = False
