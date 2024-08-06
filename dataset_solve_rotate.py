ded9a8ac352a5b382641299aa89d4203644289fd
Simon Strandgaard <neoneye@gmail.com>
2024-08-06 15:21:51 +0200

    generated new dataset

--- dataset_solve_rotate.py
+++ dataset_solve_rotate.py
@@ -106,7 +106,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=4500000,
+    seed=5500000,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
--- dataset_solve_translate.py
+++ dataset_solve_translate.py
@@ -92,7 +92,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=4000000,
+    seed=5000000,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
