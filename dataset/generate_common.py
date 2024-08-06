3523b4d6c6e413cba9afe33b2073604f397ff85e
Simon Strandgaard <neoneye@gmail.com>
2024-08-06 09:43:55 +0200

    Extracted generate_dataset_item_for_pixels_in_output_row

---
+++ dataset/generate_common.py
@@ -0,0 +1,45 @@
+import random
+from simon_arc_lab.task import *
+from simon_arc_lab.task_formatter_rle_compact import *
+from simon_arc_lab.benchmark import *
+
+def generate_dataset_item_for_pixels_in_output_row(seed: int, dataset_names: list[str], benchmark_dataset_name: str, task: Task, test_index: int, test_output_y: int, pixel_list: list[int], transformation_id: str) -> dict:
+    random.seed(seed)
+    dataset_name = random.choice(dataset_names)
+
+    task_formatter = TaskFormatterRLECompact(task)
+
+    output_ids = task_formatter.output_ids()
+    test_output_id = output_ids[task.count_examples + test_index]
+
+    instructions = [
+        f"{dataset_name}, {test_output_id}, predict row {test_output_y}",
+        f"{dataset_name} '{test_output_id}' predict row {test_output_y}",
+        f"{dataset_name} '{test_output_id}' predict the row {test_output_y}",
+        f"{dataset_name}, '{test_output_id}', predict the row {test_output_y}",
+        f"{dataset_name}, '{test_output_id}', predict y={test_output_y}",
+        f"{dataset_name} {test_output_id} predict y={test_output_y}",
+        f"{dataset_name} predict y={test_output_y} for {test_output_id}",
+        f"{dataset_name} predict row {test_output_y} for {test_output_id}",
+    ]
+    instruction = random.choice(instructions)
+
+    input = task_formatter.to_string()
+    # print(input)
+
+    output = ''.join(map(str, pixel_list))
+
+    max_width, max_height = task.max_image_size()
+    benchmark_width = image_size1d_to_string(max_width)
+    benchmark_height = image_size1d_to_string(max_height)
+    benchmark_pixels = task_pixels_to_string(task.total_pixel_count())
+    benchmark_id = f'dataset={benchmark_dataset_name} group={transformation_id} predict=pixels image_width={benchmark_width} image_height={benchmark_height} task_pixels={benchmark_pixels}'
+
+    result_dict = {
+        'instruction': instruction,
+        'input': input,
+        'output': output,
+        'benchmark': benchmark_id
+    }
+    return result_dict
+
--- dataset_solve_color.py
+++ dataset_solve_color.py
@@ -20,6 +20,7 @@ from simon_arc_lab.histogram import *
 from simon_arc_lab.benchmark import *
 from dataset.simon_solve_version1_names import SIMON_SOLVE_VERSION1_NAMES
 from dataset.plot import *
+from dataset.generate_common import *
 
 DATASET_NAMES = SIMON_SOLVE_VERSION1_NAMES
 
@@ -163,47 +164,6 @@ def demo_generate_task():
 # demo_generate_task()
 # exit()
 
-def generate_dataset_item_for_pixels_in_output_row(seed: int, task: Task, test_index: int, test_output_y: int, pixel_list: list[int], transformation_id: str) -> dict:
-    random.seed(seed)
-    dataset_name = random.choice(DATASET_NAMES)
-
-    # task_formatter = TaskFormatterRLEVerbose(task)
-    task_formatter = TaskFormatterRLECompact(task)
-
-    output_ids = task_formatter.output_ids()
-    test_output_id = output_ids[task.count_examples + test_index]
-
-    instructions = [
-        f"{dataset_name}, {test_output_id}, predict row {test_output_y}",
-        f"{dataset_name} '{test_output_id}' predict row {test_output_y}",
-        f"{dataset_name} '{test_output_id}' predict the row {test_output_y}",
-        f"{dataset_name}, '{test_output_id}', predict the row {test_output_y}",
-        f"{dataset_name}, '{test_output_id}', predict y={test_output_y}",
-        f"{dataset_name} {test_output_id} predict y={test_output_y}",
-        f"{dataset_name} predict y={test_output_y} for {test_output_id}",
-        f"{dataset_name} predict row {test_output_y} for {test_output_id}",
-    ]
-    instruction = random.choice(instructions)
-
-    input = task_formatter.to_string()
-    # print(input)
-
-    output = ''.join(map(str, pixel_list))
-
-    max_width, max_height = task.max_image_size()
-    benchmark_width = image_size1d_to_string(max_width)
-    benchmark_height = image_size1d_to_string(max_height)
-    benchmark_pixels = task_pixels_to_string(task.total_pixel_count())
-    benchmark_id = f'dataset={BENCHMARK_DATASET_NAME} group={transformation_id} predict=pixels image_width={benchmark_width} image_height={benchmark_height} task_pixels={benchmark_pixels}'
-
-    result_dict = {
-        'instruction': instruction,
-        'input': input,
-        'output': output,
-        'benchmark': benchmark_id
-    }
-    return result_dict
-
 def generate_dataset_item_for_number_of_output_rows(seed: int, task: Task, test_index: int, output_image: np.array, transformation_id: str) -> dict:
     random.seed(seed)
     dataset_name = random.choice(DATASET_NAMES)
@@ -288,6 +248,9 @@ def generate_dataset_item_for_output_image(seed: int, task: Task, test_index: in
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     random.seed(seed)
 
+    dataset_names = DATASET_NAMES
+    benchmark_dataset_name = BENCHMARK_DATASET_NAME
+
     task_without_test_output = task.clone()
     task_without_test_output.set_all_test_outputs_to_none()
 
@@ -299,7 +262,16 @@ def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: s
         output_height = output_image.shape[0]
         for output_y in range(output_height):
             pixels = image_get_row_as_list(output_image, output_y)
-            dataset_item = generate_dataset_item_for_pixels_in_output_row(seed + output_y + test_index * 100, task_without_test_output, test_index, output_y, pixels, transformation_id)
+            dataset_item = generate_dataset_item_for_pixels_in_output_row(
+                seed + output_y + test_index * 100, 
+                dataset_names, 
+                benchmark_dataset_name,
+                task_without_test_output, 
+                test_index, 
+                output_y, 
+                pixels, 
+                transformation_id
+            )
             dataset_items.append(dataset_item)
 
     # Predict the number of rows in the output image
