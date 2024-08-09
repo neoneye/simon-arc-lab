3f91fd875225a53c4e9d8ff486f858e0c34302eb
Simon Strandgaard <neoneye@gmail.com>
2024-08-09 23:41:44 +0200

    compare the predicted with the expected.

---
+++ simon_arc_lab/image_comparison_matplotlib.py
@@ -0,0 +1,51 @@
+# Based on plot_xyt(), plot_single_image() by Minseo Kim
+# https://www.kaggle.com/code/minseo14/arc-task-00d62c1b-with-cnn
+from .task import Task
+from typing import Optional
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import numpy as np
+import os
+
+ARCAGI_COLORS = [
+    '#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00',
+    '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'
+]
+
+GRID_COLOR = '#555555'
+IMAGE_BORDER_COLOR = GRID_COLOR
+LABEL_COLOR_TRAIN_PAIR = '#444'
+LABEL_COLOR_TEST_PAIR = '#222'
+TASK_BACKGROUND_COLOR = '#dddddd'
+
+def plot_single_image(image: np.array, ax, title: str, cmap, norm):
+    ax.imshow(image, cmap=cmap, norm=norm)
+    ax.grid(True, which='both', color=GRID_COLOR, linewidth = 1)
+    
+    plt.setp(plt.gcf().get_axes(), xticklabels=[], yticklabels=[])
+    ax.set_xticks([x-0.5 for x in range(1 + len(image[0]))])     
+    ax.set_yticks([x-0.5 for x in range(1 + len(image))])
+
+    ax.tick_params(top=False, bottom=False, left=False, right=False, labelleft=False, labelbottom=False)
+    
+    ax.set_title(title, fontweight='bold')
+
+def plot_xyt(input_image: np.array, predicted_image: np.array, expected_image: Optional[np.array], title: str):
+    """Plots the input, predicted, and answer pairs of a specified task, using the ARC color scheme."""
+    num_img = 3
+    fig, axs = plt.subplots(1, num_img, figsize=(9, num_img))
+    plt.suptitle(title, fontsize=20, fontweight='bold', y=0.96)
+    
+    cmap = colors.ListedColormap(ARCAGI_COLORS)
+    norm = colors.Normalize(vmin=0, vmax=9)
+    
+    plot_single_image(input_image, axs[0], 'Input', cmap, norm)
+    plot_single_image(predicted_image, axs[1], 'Predicted', cmap, norm)
+    if expected_image is not None:
+        plot_single_image(expected_image, axs[2], 'Answer', cmap, norm)
+    
+    fig.patch.set_facecolor('#dddddd')
+
+    fig.tight_layout()
+    plt.show()
+
---
+++ simon_arc_model_run/run_tasks.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, PROJECT_ROOT)
+
+from simon_arc_lab.image_util import *
+from simon_arc_lab.task import *
+from simon_arc_lab.taskset import TaskSet
+from simon_arc_lab.image_comparison_matplotlib import plot_xyt
+from simon_arc_lab.task_formatter_rle_compact import *
+from simon_arc_lab.rle.serialize import *
+from simon_arc_lab.rle.deserialize import *
+from simon_arc_model.model import Model
+
+def process_task(task: Task, model: Model):
+    task_without_test_output = task.clone()
+    task_without_test_output.set_all_test_outputs_to_none()
+
+    task_formatter = TaskFormatterRLECompact(task_without_test_output)
+    output_ids = task_formatter.output_ids()
+
+    dataset_name = 'SIMON-SOLVE-V1'
+
+    # Predict the entire image of the test output image
+    for test_index in range(task.count_tests):
+        input = task_formatter.to_string()
+        input_image = task.test_input(test_index)
+        expected_output_image = task.test_output(test_index)
+        test_output_id = output_ids[task_without_test_output.count_examples + test_index]
+
+        prompt = f"{dataset_name}, {test_output_id}, predict image\n{input}"
+        response = model.process(prompt)
+        predicted_output_image = deserialize(response)
+
+        is_correct = np.array_equal(predicted_output_image, expected_output_image)
+
+        correct_incorrect = 'correct' if is_correct else 'incorrect'
+        title = f'{task.metadata_task_id} test={test_index} {correct_incorrect}'
+
+        plot_xyt(input_image, predicted_output_image, expected_output_image, title)
+
+
+model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model168'
+
+# path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/ARC/data/training'
+path_to_task_dir = os.path.join(PROJECT_ROOT, 'testdata', 'simple_arc_tasks')
+taskset = TaskSet.load_directory(path_to_task_dir)
+
+# Load model
+model = Model(model_directory, 512)
+
+for task in taskset.tasks:
+    if task.total_pixel_count() > 500:
+        continue
+    process_task(task, model)
