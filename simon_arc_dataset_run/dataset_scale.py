b4ac5d92a3e93dfcaa1365e360e22f2a2b177a00
Simon Strandgaard <neoneye@gmail.com>
2024-08-11 17:22:39 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_scale.py
+++ simon_arc_dataset_run/dataset_scale.py
@@ -45,7 +45,7 @@ def generate_dataset_item_with_scale(seed: int) -> dict:
     :return: A dictionary with the instruction, input, and output
     """
     min_image_size = 1
-    max_image_size = 5
+    max_image_size = 30
 
     min_scale_factor = 1
     max_scale_factor = 3
@@ -65,8 +65,9 @@ def generate_dataset_item_with_scale(seed: int) -> dict:
     for _ in range(100):
         scalex_factor = random.randint(min_scale_factor, max_scale_factor)
         scaley_factor = random.randint(min_scale_factor, max_scale_factor)
-        if scalex_factor != 1 and scaley_factor != 1:
-            break
+        if scalex_factor == 1 and scaley_factor == 1:
+            continue
+        break
     if scalex_factor == 1 and scaley_factor == 1:
         if seed % 2 == 0:
             scalex_factor = 1
@@ -75,11 +76,24 @@ def generate_dataset_item_with_scale(seed: int) -> dict:
             scalex_factor = 2
             scaley_factor = 1
 
-    instructions = [
+    instructions_scale_both_x_and_y = [
         f'{dataset_name} scale-x {scalex_up_down} by {scalex_factor}, scale-y {scaley_up_down} by {scaley_factor}',
         f'{dataset_name} scalex={scalex_up_down}{scalex_factor} scaley={scaley_up_down}{scaley_factor}',
     ]
+    instructions_scale_x_with_y1 = [
+        f'{dataset_name} scale-x {scalex_up_down} by {scalex_factor}',
+        f'{dataset_name} scalex={scalex_up_down}{scalex_factor}',
+    ]
+    instructions_scale_y_with_x1 = [
+        f'{dataset_name} scale-y {scaley_up_down} by {scaley_factor}',
+        f'{dataset_name} scaley={scaley_up_down}{scaley_factor}',
+    ]
 
+    instructions = instructions_scale_both_x_and_y
+    if scalex_factor == 1:
+        instructions = instructions_scale_y_with_x1
+    if scaley_factor == 1:
+        instructions = instructions_scale_x_with_y1
     instruction = random.choice(instructions)
 
     unscaled_image = image_create_random_advanced(seed + 5, min_image_size, max_image_size, min_image_size, max_image_size)
@@ -87,20 +101,21 @@ def generate_dataset_item_with_scale(seed: int) -> dict:
     # Input image
     input_image = unscaled_image.copy()
     if scalex_up_down == 'down':
-        input_image = np.kron(input_image, np.ones((1, scalex_factor)))
+        input_image = np.kron(input_image, np.ones((1, scalex_factor))).astype(np.uint8)
     if scaley_up_down == 'down':
-        input_image = np.kron(input_image, np.ones((scaley_factor, 1)))
+        input_image = np.kron(input_image, np.ones((scaley_factor, 1))).astype(np.uint8)
 
     # Output image
     output_image = unscaled_image.copy()
     if scalex_up_down == 'up':
-        output_image = np.kron(output_image, np.ones((1, scalex_factor)))
+        output_image = np.kron(output_image, np.ones((1, scalex_factor))).astype(np.uint8)
     if scaley_up_down == 'up':
-        output_image = np.kron(output_image, np.ones((scaley_factor, 1)))
+        output_image = np.kron(output_image, np.ones((scaley_factor, 1))).astype(np.uint8)
 
-    if True:
+    if False:
         print(f"---\ninput: {input_image}\nscale-x {scalex_up_down} by {scalex_factor}\nscale-y {scaley_up_down} by {scaley_factor}\noutput: {output_image}")
-        title = f'scale-x {scalex_up_down} by {scalex_factor}, scale-y {scaley_up_down} by {scaley_factor}'
+        # title = f'scale-x {scalex_up_down} by {scalex_factor}, scale-y {scaley_up_down} by {scaley_factor}'
+        title = instruction
         show_prediction_result(input_image, output_image, None, title, show_grid=True, save_path=None)
 
     input = serialize(input_image)
@@ -130,7 +145,7 @@ generator = DatasetGenerator(
 )
 generator.generate(
     seed=1003003,
-    max_num_samples=5,
+    max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
 # generator.inspect()
