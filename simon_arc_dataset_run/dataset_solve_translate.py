e7ad42af56f6c1cd1499216dede8ca3aac4a399d
Simon Strandgaard <neoneye@gmail.com>
2024-08-13 12:01:50 +0200

    Increased translation distance from 1px to 2px.

--- simon_arc_dataset_run/dataset_solve_translate.py
+++ simon_arc_dataset_run/dataset_solve_translate.py
@@ -1,6 +1,4 @@
-# Translate the image by 1 pixel, up/down/left/right.
-#
-# IDEA: Translate by 2 pixels.
+# Translate the image by -2..+2 pixel, up/down/left/right.
 #
 # Present the same input images, but with different transformations.
 # so from the examples alone, the model have to determine what happened.
@@ -32,14 +30,12 @@ def generate_task(seed: int, dx: int, dy: int, percent_noise: float) -> Task:
     count_test = random.Random(seed + 2).randint(1, 2)
     # count_test = 1
     task = Task()
-    min_width = 3
-    max_width = 10
-    min_height = 3
-    max_height = 10
+    min_image_size = 1
+    max_image_size = 8
 
     for i in range(count_example+count_test):
         is_example = i < count_example
-        input_image = image_create_random_advanced(seed + 1000 + i, min_width, max_width, min_height, max_height)
+        input_image = image_create_random_advanced(seed + 1000 + i, min_image_size, max_image_size, min_image_size, max_image_size)
 
         transformed_image = image_translate_wrap(input_image, dx, dy)
 
@@ -72,22 +68,52 @@ def generate_dataset_item_list(seed: int) -> list[dict]:
 
     seed_task = seed
 
-    directions = [
-        (1, 0, 'translate_xplus1'), 
-        (-1, 0, 'translate_xminus1'),
-        (0, 1, 'translate_yplus1'), 
-        (0, -1, 'translate_yminus1'),
-        (1, -1, 'translate_xplus1yminus1'), 
-        (-1, -1, 'translate_xminus1minus1'),
-        (1, 1, 'translate_xplus1yplus1'), 
-        (-1, 1, 'translate_xminus1plus1'),
-    ]
+    max_translate = 2
+
+    # Populate a config_list like this 
+    # [(-1, -1, 'translate_xminus1yminus1'), (0, -1, 'translate_yminus1'), (1, -1, 'translate_xplus1yminus1'), 
+    #  (-1, 0, 'translate_xminus1'), (1, 0, 'translate_xplus1'), (-1, 1, 'translate_xminus1yplus1'), 
+    #  (0, 1, 'translate_yplus1'), (1, 1, 'translate_xplus1yplus1')]
+    config_list = []
+    for dy in range(-max_translate, max_translate+1):
+        for dx in range(-max_translate, max_translate+1):
+            if dx == 0 and dy == 0:
+                continue
+            name_x = 'zero'
+            if dx > 0:
+                name_x = f'xplus{dx}'
+            else:
+                name_x = f'xminus{-dx}'
+            name_y = 'zero'
+            if dy > 0:
+                name_y = f'yplus{dy}'
+            else:
+                name_y = f'yminus{-dy}'
+            if dx == 0:
+                transformation_id = f'translate_{name_y}'
+            elif dy == 0:
+                transformation_id = f'translate_{name_x}'
+            else:
+                transformation_id = f'translate_{name_x}{name_y}'
+            config_list.append((dx, dy, transformation_id))
+
+#    print(f'config_list: {config_list}')
+
+    # shuffle the parameters
+    random.shuffle(config_list)
+
+    # truncate the parameters to a few
+    truncate_length = random.randint(2, 5)
+    config_list_truncated = config_list[:truncate_length]
+
+    # print(f'truncate_length: {truncate_length} config_list_truncated: {config_list_truncated}')
 
     all_dataset_items = []
-    for direction in directions:
-        dx, dy, transformation_id = direction
+    for config in config_list_truncated:
+        dx, dy, transformation_id = config
         percent_noise = 0.0
         task = generate_task(seed_task, dx, dy, percent_noise)
+        # task.metadata_task_id = transformation_id
         # task.show()
         dataset_items = generate_dataset_item_list_inner(seed, task, transformation_id)
         all_dataset_items.extend(dataset_items)
@@ -98,7 +124,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=21000005,
+    seed=31000005,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
