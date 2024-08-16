b9e81efe22dbf04ad7e593d950437aa065635f76
Simon Strandgaard <neoneye@gmail.com>
2024-08-16 22:38:29 +0200

    Process multiple ARC datasets.

--- simon_arc_model_run/run_tasks.py
+++ simon_arc_model_run/run_tasks.py
@@ -19,21 +19,38 @@ if not os.path.isdir(model_directory):
     print(f"Model directory '{model_directory}' does not exist.")
     sys.exit(1)
 
-path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/ARC/data/evaluation'
-# path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/ARC/data/training'
-# path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/Mini-ARC/data'
-# path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/ConceptARC/data'
-# path_to_task_dir = os.path.join(PROJECT_ROOT, 'testdata', 'simple_arc_tasks')
-taskset = TaskSet.load_directory(path_to_task_dir)
+path_to_arc_dataset_collection_dataset = '/Users/neoneye/git/arc-dataset-collection/dataset'
+if not os.path.isdir(path_to_arc_dataset_collection_dataset):
+    print(f"ARC dataset collection directory '{path_to_arc_dataset_collection_dataset}' does not exist.")
+    sys.exit(1)
 
+groupname_pathtotaskdir_list = [
+    ('arcagi_training', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/training')),
+    ('arcagi_evaluation', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/evaluation')),
+    ('miniarc', os.path.join(path_to_arc_dataset_collection_dataset, 'Mini-ARC/data')),
+    ('conceptarc', os.path.join(path_to_arc_dataset_collection_dataset, 'ConceptARC/data')),
+    # ('testdata', os.path.join(PROJECT_ROOT, 'testdata', 'simple_arc_tasks')),
+]
+
+for groupname, path_to_task_dir in groupname_pathtotaskdir_list:
+    if not os.path.isdir(path_to_task_dir):
+        print(f"path_to_task_dir directory '{path_to_task_dir}' does not exist.")
+        sys.exit(1)
 
 # Load model
 model = Model(model_directory, 512)
 
-wm = WorkManager(model, taskset)
-wm.discard_items_with_too_long_prompts(500)
-# wm.process_all_work_items()
-wm.process_all_work_items(save_dir='run_tasks_result')
-# wm.process_all_work_items(show=True)
-wm.summary()
-wm.save_arcprize2024_submission_file('submission.json')
+number_of_items_in_list = len(groupname_pathtotaskdir_list)
+for index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotaskdir_list):
+    save_dir = f'run_tasks_result/{model_iteration}/{groupname}'
+    print(f"Processing {index+1} of {number_of_items_in_list}. Group name '{groupname}'. Results will be saved to '{save_dir}'")
+
+    taskset = TaskSet.load_directory(path_to_task_dir)
+
+    wm = WorkManager(model, taskset)
+    wm.discard_items_with_too_long_prompts(500)
+    # wm.process_all_work_items()
+    wm.process_all_work_items(save_dir=save_dir)
+    # wm.process_all_work_items(show=True)
+    wm.summary()
+    # wm.save_arcprize2024_submission_file('submission.json')
