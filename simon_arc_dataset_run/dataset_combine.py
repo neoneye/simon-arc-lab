0ac83d622ce95ae6229688b7dd5e7211a38da190
Simon Strandgaard <neoneye@gmail.com>
2024-08-16 18:44:56 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_combine.py
+++ simon_arc_dataset_run/dataset_combine.py
@@ -5,7 +5,7 @@ import random
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, PROJECT_ROOT)
 
-seed = 78
+seed = 79
 random.seed(seed)
 
 # Define the input file paths
