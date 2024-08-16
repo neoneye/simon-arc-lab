41c38f1610116f452aea6f1dd0845d4d2f29260c
Simon Strandgaard <neoneye@gmail.com>
2024-08-16 13:43:58 +0200

    trying newer models

--- simon_arc_model_run/run_tasks.py
+++ simon_arc_model_run/run_tasks.py
@@ -10,13 +10,20 @@ from simon_arc_model.work_manager import WorkManager
 
 # model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model229' # training=21 evaluation=6
 # model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model231'
-model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model232'
+# model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model232'
+# model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model233'
+# model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model235'
+# model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model237'
+# model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model239'
+# model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model240' # training=23 evaluation=4
+# model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model241'
+model_directory = '/Users/neoneye/nobackup/git/simon-arc-lab-model242'
 
 path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/ARC/data/training'
 # path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/ARC/data/evaluation'
 # path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/Mini-ARC/data'
 # path_to_task_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/ConceptARC/data'
-path_to_task_dir = os.path.join(PROJECT_ROOT, 'testdata', 'simple_arc_tasks')
+# path_to_task_dir = os.path.join(PROJECT_ROOT, 'testdata', 'simple_arc_tasks')
 taskset = TaskSet.load_directory(path_to_task_dir)
 
 
