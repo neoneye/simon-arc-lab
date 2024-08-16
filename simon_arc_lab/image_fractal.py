277399867b776a2fe651adf761524b527840fa0c
Simon Strandgaard <neoneye@gmail.com>
2024-08-16 15:37:05 +0200

    image_fractal_from_mask_and_image() added

--- simon_arc_lab/image_fractal.py
+++ simon_arc_lab/image_fractal.py
@@ -9,18 +9,36 @@ def image_fractal_from_mask(mask: np.ndarray) -> np.ndarray:
 
     The output image size is the input size squared.
 
-    :param mask: The input mask image.
+    :param mask: The input mask.
     :return: The output fractal image. 
     """
-    input_height, input_width = mask.shape
-    output_width = input_width * input_width
-    output_height = input_height * input_height
-    output_image = np.zeros((output_height, output_width), dtype=np.uint8)
-    for y in range(input_height):
-        for x in range(input_width):
+    empty_color = 0
+    return image_fractal_from_mask_and_image(mask, mask, empty_color)
+
+def image_fractal_from_mask_and_image(mask: np.ndarray, image: np.ndarray, empty_color: int) -> np.ndarray:
+    """
+    Create a self-similar fractal mask from a mask image.
+
+    Everywhere the input mask is 1, the entire input image is copied to the output image.
+    Everywhere the input mask is 0, the output image is set to the empty_color.
+
+    The output image size is the input mask x input image.
+
+    :param mask: The input mask image.
+    :param image: The input image to be inserted.
+    :param empty_color: The background color for areas where the mask is 0.
+    :return: The output fractal image. 
+    """
+    input_mask_height, input_mask_width = mask.shape
+    input_image_height, input_image_width = mask.shape
+    output_width = input_mask_width * input_image_width
+    output_height = input_mask_height * input_image_height
+    output_image = np.full((output_height, output_width), empty_color, dtype=np.uint8)
+    for y in range(input_mask_height):
+        for x in range(input_mask_width):
             if mask[y, x] > 0:
-                for dy in range(input_height):
-                    for dx in range(input_width):
-                        output_image[y * input_height + dy, x * input_width + dx] = mask[dy, dx]
+                for dy in range(input_image_height):
+                    for dx in range(input_image_width):
+                        output_image[y * input_image_height + dy, x * input_image_width + dx] = image[dy, dx]
 
     return output_image
--- simon_arc_lab/test_image_fractal.py
+++ simon_arc_lab/test_image_fractal.py
@@ -41,3 +41,22 @@ class TestImageFractal(unittest.TestCase):
             [0, 0, 0,  0, 0, 0,  1, 0, 0],
             [0, 0, 0,  0, 0, 0,  0, 0, 1]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
+
+    def test_20000_image_fractal_from_mask_and_image(self):
+        # Arrange
+        mask = np.array([
+            [1, 0, 0],
+            [0, 0, 1]], dtype=np.uint8)
+        image = np.array([
+            [1, 2, 3],
+            [4, 5, 6]], dtype=np.uint8)
+        # Act
+        actual = image_fractal_from_mask_and_image(mask, image, 9)
+        # Assert
+        expected = np.array([
+            [1, 2, 3,  9, 9, 9,  9, 9, 9],
+            [4, 5, 6,  9, 9, 9,  9, 9, 9],
+
+            [9, 9, 9,  9, 9, 9,  1, 2, 3],
+            [9, 9, 9,  9, 9, 9,  4, 5, 6]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
