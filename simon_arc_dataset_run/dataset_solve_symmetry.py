cf062f59ba7a9d58ccd4b36dd44881a25b291bca
Simon Strandgaard <neoneye@gmail.com>
2024-08-20 21:30:55 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_symmetry.py
+++ simon_arc_dataset_run/dataset_solve_symmetry.py
@@ -347,8 +347,9 @@ def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: s
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
-    # j = seed % 4
-    j = (seed % 2) + 2
+    j = seed % 4
+    # j = (seed % 2) + 2
+    # j = (seed % 2)
     if j == 0:
         task = generate_task_with_input_image_create_output_symmetry_rect(seed)
         task_id = task.metadata_task_id
@@ -373,7 +374,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=1718000410,
+    seed=1818000410,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
