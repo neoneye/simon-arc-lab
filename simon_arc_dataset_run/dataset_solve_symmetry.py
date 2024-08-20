4c7c2299713a12741bba0694f7a0a2fc2a36b468
Simon Strandgaard <neoneye@gmail.com>
2024-08-20 11:04:22 +0200

    generate square symmetry patterns with padding

--- simon_arc_dataset_run/dataset_solve_symmetry.py
+++ simon_arc_dataset_run/dataset_solve_symmetry.py
@@ -41,6 +41,9 @@ from simon_arc_lab.image_create_random_simple import *
 from simon_arc_lab.image_symmetry import *
 from simon_arc_lab.histogram import Histogram
 from simon_arc_lab.benchmark import *
+from simon_arc_lab.image_pad import image_pad_random
+from simon_arc_lab.image_trim import outer_bounding_box_after_trim_with_color
+from simon_arc_lab.rectangle import Rectangle
 from simon_arc_dataset.simon_solve_version1_names import SIMON_SOLVE_VERSION1_NAMES
 from simon_arc_dataset.generate_solve import *
 from simon_arc_dataset.dataset_generator import *
@@ -84,13 +87,27 @@ def generate_task_with_input_image_create_output_symmetry_square(seed: int) -> T
     min_image_size = 2
     max_image_size = 4
 
-    image_symmetry = ImageSymmetrySquare.create_random(seed * 1333 + 100)
+    is_padded = random.Random(seed + 16).choice([False, True])
+    max_pad_count = 5
+    color_padding = random.Random(seed + 17).randint(0, 9)
+
+    pattern_ids = [ImageSymmetryPatternId.HSTACK2, ImageSymmetryPatternId.VSTACK2]
+    pattern_id = random.Random(seed + 773).choice(pattern_ids)
+
+    image_symmetry = ImageSymmetrySquare(pattern_id)
+    # image_symmetry = ImageSymmetrySquare.create_random(seed * 1333 + 100)
     image_symmetry.randomize_name_list(seed * 8773 + 2)
+
     instruction_sequence = image_symmetry.instruction_sequence()
-    task.metadata_task_id = "square " + instruction_sequence
+    if is_padded:
+        task_id_padding_text = ' padded'
+    else:
+        task_id_padding_text = ''
+    task.metadata_task_id = "square " + instruction_sequence + task_id_padding_text
 
     for i in range(count_example+count_test):
         is_example = i < count_example
+        output_image = None
         input_image = None
         for retry_index in range(100):
             seed_for_input_image_size = seed * 5 + retry_index * 133 + i
@@ -98,12 +115,28 @@ def generate_task_with_input_image_create_output_symmetry_square(seed: int) -> T
             image_size = random.Random(seed_for_input_image_size).randint(min_image_size, max_image_size)
             candidate_image = image_create_random_advanced(seed_for_input_image_data, image_size, image_size, image_size, image_size)
             histogram = Histogram.create_with_image(candidate_image)
-            if histogram.number_of_unique_colors() > 1:
-                input_image = candidate_image
-                break
+            if histogram.number_of_unique_colors() < 2:
+                continue
+            input_image = candidate_image
+            if is_padded:
+                height, width = input_image.shape
+                input_image = image_pad_random(input_image, seed=seed * 11 + 1000 + i * 997, color=color_padding, min_pad_count=1, max_pad_count=max_pad_count)
+                # Reject the image if the padding color conflicts with the tile color, so it's impossible to extract the tile.
+                rect = outer_bounding_box_after_trim_with_color(input_image, color_padding)
+                if rect.is_empty():
+                    continue
+                if rect.width != width or rect.height != height:
+                    # print("ambiguous padding")
+                    # The trimmed image doesn't have the same dimensions as the input_image, then there is the padding color
+                    # occurs too many times in the input_image making it difficult to extract the tile.
+                    continue
+
+            output_image = image_symmetry.execute(candidate_image)
+            break
         if input_image is None:
             raise Exception("Failed to create a square image with more than one color")
-        output_image = image_symmetry.execute(input_image)
+        if output_image is None:
+            raise Exception("Failed to create a square image with more than one color")
         task.append_pair(input_image, output_image, is_example)
 
     return task
@@ -151,34 +184,64 @@ def generate_task_with_symmetry_square_input_image_and_extract_a_particular_tile
     min_image_size = 2
     max_image_size = 4
 
-    image_symmetry = ImageSymmetryRect.create_random(seed * 1333 + 100)
+    is_padded = random.Random(seed + 16).choice([False, True])
+    max_pad_count = 5
+    color_padding = random.Random(seed + 17).randint(0, 9)
+
+    pattern_ids = [ImageSymmetryPatternId.HSTACK2, ImageSymmetryPatternId.VSTACK2]
+    pattern_id = random.Random(seed + 773).choice(pattern_ids)
+
+    image_symmetry = ImageSymmetrySquare(pattern_id)
+    # image_symmetry = ImageSymmetrySquare.create_random(seed * 1333 + 100)
     image_symmetry.randomize_name_list(seed * 8773 + 2)
 
     # It's the top-left tile that is always extracted. It's the first tile.
     image_symmetry.use_mutation_for_index(0, ImageSymmetryMutationId.ORIGINAL)
 
     instruction_sequence = image_symmetry.instruction_sequence()
-    task.metadata_task_id = "square " + instruction_sequence
+    if is_padded:
+        task_id_padding_text = ' padded'
+    else:
+        task_id_padding_text = ''
+    task.metadata_task_id = "square " + instruction_sequence + task_id_padding_text
 
     # Rotate the image, so the tile is not always at the top-left, but also in the other corners.
     rotate90_count = random.Random(seed + 3).randint(0, 3)
     for i in range(count_example+count_test):
         is_example = i < count_example
-        output_image_raw = None
+        output_image = None
+        input_image = None
         for retry_index in range(100):
             seed_for_input_image_size = seed * 5 + retry_index * 133 + i
             seed_for_input_image_data = seed * 7 + retry_index * 133 + i
             image_size = random.Random(seed_for_input_image_size).randint(min_image_size, max_image_size)
             candidate_image = image_create_random_advanced(seed_for_input_image_data, image_size, image_size, image_size, image_size)
             histogram = Histogram.create_with_image(candidate_image)
-            if histogram.number_of_unique_colors() > 1:
-                output_image_raw = candidate_image
-                break
-        if output_image_raw is None:
+            if histogram.number_of_unique_colors() < 2:
+                continue
+            output_image_raw = candidate_image
+            input_image_raw = image_symmetry.execute(output_image_raw)
+
+            input_image = np.rot90(input_image_raw, rotate90_count)
+            if is_padded:
+                height, width = input_image.shape
+                input_image = image_pad_random(input_image, seed=seed * 11 + 1000 + i * 997, color=color_padding, min_pad_count=1, max_pad_count=max_pad_count)
+                # Reject the image if the padding color conflicts with the tile color, so it's impossible to extract the tile.
+                rect = outer_bounding_box_after_trim_with_color(input_image, color_padding)
+                if rect.is_empty():
+                    continue
+                if rect.width != width or rect.height != height:
+                    # print("ambiguous padding")
+                    # The trimmed image doesn't have the same dimensions as the input_image, then there is the padding color
+                    # occurs too many times in the input_image making it difficult to extract the tile.
+                    continue
+
+            output_image = np.rot90(output_image_raw, rotate90_count)
+            break
+        if input_image is None:
             raise Exception("Failed to create a square image with more than one color")
-        input_image_raw = image_symmetry.execute(output_image_raw)
-        input_image = np.rot90(input_image_raw, rotate90_count)
-        output_image = np.rot90(output_image_raw, rotate90_count)
+        if output_image is None:
+            raise Exception("Failed to create a square image with more than one color")
         task.append_pair(input_image, output_image, is_example)
 
     return task
@@ -206,7 +269,8 @@ def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: s
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
-    j = seed % 4
+    # j = seed % 4
+    j = (seed % 2) + 2
     if j == 0:
         task = generate_task_with_input_image_create_output_symmetry_rect(seed)
         task_id = task.metadata_task_id
@@ -231,7 +295,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=718000410,
+    seed=918000410,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
