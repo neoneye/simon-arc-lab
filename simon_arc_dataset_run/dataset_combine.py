204476e112470ce107cf34b3fcb9b26f29d915a5
Simon Strandgaard <neoneye@gmail.com>
2024-08-21 20:44:21 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_combine.py
+++ simon_arc_dataset_run/dataset_combine.py
@@ -5,7 +5,7 @@ import random
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, PROJECT_ROOT)
 
-seed = 101
+seed = 102
 random.seed(seed)
 
 # Define the input file paths
@@ -24,6 +24,7 @@ file_names = [
     'dataset_solve_compress.jsonl',
     'dataset_solve_erosion.jsonl',
     'dataset_solve_fractal.jsonl',
+    'dataset_solve_grid.jsonl',
     'dataset_solve_outline.jsonl',
     'dataset_solve_rotate.jsonl',
     'dataset_solve_scale.jsonl',
