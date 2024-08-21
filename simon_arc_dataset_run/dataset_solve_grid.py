919b33ed5044b2f25ff978b2662eef88c68bd873
Simon Strandgaard <neoneye@gmail.com>
2024-08-21 23:25:26 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_grid.py
+++ simon_arc_dataset_run/dataset_solve_grid.py
@@ -309,7 +309,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=61300013,
+    seed=71300013,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
--- simon_arc_dataset_run/dataset_solve_zindex.py
+++ simon_arc_dataset_run/dataset_solve_zindex.py
@@ -252,7 +252,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=147100911,
+    seed=157100911,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
