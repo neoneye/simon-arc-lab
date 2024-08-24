c86bca3d6260a5029d3ba8de5ef465af92a8e102
Simon Strandgaard <neoneye@gmail.com>
2024-08-24 10:55:43 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_edge.py
+++ simon_arc_dataset_run/dataset_solve_edge.py
@@ -140,7 +140,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=150999999,
+    seed=160999999,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
