487fe0029289c6bc4608e14d69206d784eaa33a1
Simon Strandgaard <neoneye@gmail.com>
2024-08-25 21:31:00 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_mask.py
+++ simon_arc_dataset_run/dataset_solve_mask.py
@@ -167,7 +167,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=120033371,
+    seed=130033371,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
