e3adb67a9470408f7dadf76346e69a22c3ab8129
Simon Strandgaard <neoneye@gmail.com>
2024-08-26 13:35:27 +0200

    image_rect_hollow() added

--- simon_arc_lab/image_rect.py
+++ simon_arc_lab/image_rect.py
@@ -26,3 +26,36 @@ def image_rect(image: np.array, rect: Rectangle, color: int) -> np.array:
             new_image[y, x] = color
 
     return new_image
+
+def image_rect_hollow(image: np.array, rect: Rectangle, color: int, size: int) -> np.array:
+    """
+    Draw a hollow rectangle on an image.
+
+    :param image: The image to draw the rect on
+    :param rect: The coordinates of the rectangle
+    :param color: The color to filled with
+    :param size: The size of the rectangle border
+    :return: The image with the rectangle drawn on it
+    """
+    height, width = image.shape
+    this_image_rect = Rectangle(0, 0, width, height)
+    intersection_rect = rect.intersection(this_image_rect)
+
+    hollow_width = rect.width - size * 2
+    hollow_height = rect.height - size * 2
+    hollow_x = rect.x + size
+    hollow_y = rect.y + size
+
+    # Clone the image to avoid mutating the original
+    new_image = np.copy(image)
+
+    # Check if the coordinates are outside the image bounds
+    if intersection_rect.is_empty():
+        return np.copy(image)
+
+    for y in range(intersection_rect.y, intersection_rect.y + intersection_rect.height):
+        for x in range(intersection_rect.x, intersection_rect.x + intersection_rect.width):
+            if x < hollow_x or x >= hollow_x + hollow_width or y < hollow_y or y >= hollow_y + hollow_height:
+                new_image[y, x] = color
+
+    return new_image
--- simon_arc_lab/test_image_rect.py
+++ simon_arc_lab/test_image_rect.py
@@ -43,3 +43,50 @@ class TestImageRect(unittest.TestCase):
             [2, 2, 2, 2, 2],
             [2, 2, 2, 2, 2]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
+
+    def test_20000_image_rect_hollow_all_of_rect_is_inside_size1(self):
+        # Arrange
+        image = np.array([
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9]], dtype=np.uint8)
+        # Act
+        actual = image_rect_hollow(image, Rectangle(1, 2, 3, 4), 2, 1)
+        # Assert
+        expected = np.array([
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 2, 2, 2, 9, 9, 9, 9],
+            [9, 2, 9, 2, 9, 9, 9, 9],
+            [9, 2, 9, 2, 9, 9, 9, 9],
+            [9, 2, 2, 2, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_20001_image_rect_hollow_all_of_rect_is_inside_size2(self):
+        # Arrange
+        image = np.array([
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9]], dtype=np.uint8)
+        # Act
+        actual = image_rect_hollow(image, Rectangle(1, 2, 5, 6), 2, 2)
+        # Assert
+        expected = np.array([
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9, 9],
+            [9, 2, 2, 2, 2, 2, 9, 9],
+            [9, 2, 2, 2, 2, 2, 9, 9],
+            [9, 2, 2, 9, 2, 2, 9, 9],
+            [9, 2, 2, 9, 2, 2, 9, 9],
+            [9, 2, 2, 2, 2, 2, 9, 9]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
