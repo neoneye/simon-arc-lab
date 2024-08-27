20036c6008d2b5b9b2027ae82d7f798f9c9226e9
Simon Strandgaard <neoneye@gmail.com>
2024-08-27 13:38:03 +0200

    Added noise to the images being scaled down.

--- simon_arc_dataset_run/dataset_solve_scale.py
+++ simon_arc_dataset_run/dataset_solve_scale.py
@@ -3,6 +3,9 @@
 # Model has been trained with max_image_size: 1-20. Model needs to be train with max_image_size greater than 20.
 # Model has been trained with max_scale_factor: 1-7. Model needs to be train with max_scale_factor greater than 7.
 #
+# IDEA: add noise to the image being down scaled.
+# https://neoneye.github.io/arc/edit.html?dataset=ARC&task=5614dbcf
+#
 # Present the same input images, but with different transformations.
 # so from the examples alone, the model have to determine what happened.
 import os
@@ -38,6 +41,34 @@ def compute_max_image_size(max_image_size: int, scale_factor: int) -> int:
     # print(f"scale_factor: {scale_factor} computed_max_image_size {computed_max_image_size}")
     return computed_max_image_size
 
+def scale_up_and_add_noise(unscaled_image: np.array, seed: int, scale_x: int, scale_y: int, noise_color: Optional[int]) -> np.array:
+    height, width = unscaled_image.shape
+    result_image = np.zeros((height * scale_y, width * scale_x), dtype=np.int32)
+    for y in range(height):
+        for x in range(width):
+            value = unscaled_image[y, x]
+            for y_offset in range(scale_y):
+                for x_offset in range(scale_x):
+                    result_image[y*scale_y + y_offset, x*scale_x + x_offset] = value
+    positions = []
+    for y_offset in range(scale_y):
+        for x_offset in range(scale_x):
+            positions.append((x_offset, y_offset))
+
+    for y in range(height):
+        for x in range(width):
+            positions_copy = positions.copy()
+            random.Random(seed + y * 100 + x).shuffle(positions_copy)
+            count_positions = random.Random(seed + y * 100 + x + 1).randint(0, 1)
+            for i in range(count_positions):
+                x_offset, y_offset = positions_copy[i]
+                if noise_color is None:
+                    color = random.Random(seed + y * 10000 + x * 3939 + 23838 + i * 38382).randint(0, 9)
+                else:
+                    color = noise_color
+                result_image[y*scale_y + y_offset, x*scale_x + x_offset] = color
+
+    return result_image
 
 def generate_task(seed: int, x_up_down, x_scale, y_up_down, y_scale) -> Task:
     count_example = random.Random(seed + 1).randint(2, 4)
@@ -45,9 +76,29 @@ def generate_task(seed: int, x_up_down, x_scale, y_up_down, y_scale) -> Task:
     # count_test = 1
     task = Task()
     min_image_size = 1
-    max_image_size = 30
+    max_image_size = 10
 
-    task.metadata_task_id = f'scale_x{x_up_down}{x_scale}_y{y_up_down}{y_scale}'
+    can_add_noise = False
+    if x_up_down == 'down' and y_up_down == 'down':
+        if x_scale > 1 and y_scale > 1:
+            can_add_noise = True
+
+    noise_color = None
+    if can_add_noise:
+        # half of the time the same color is used
+        # half of the time, it's a random color
+        noise_colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, None, None, None, None, None, None, None, None, None, None]
+        noise_color = random.Random(seed + 3).choice(noise_colors)
+
+    if can_add_noise:
+        if noise_color is None:
+            noise_str = f'_noiserandom'
+        else:
+            noise_str = f'_noise{noise_color}'
+    else:
+        noise_str = ''
+
+    task.metadata_task_id = f'scale_x{x_up_down}{x_scale}_y{y_up_down}{y_scale}{noise_str}'
 
     for i in range(count_example+count_test):
         is_example = i < count_example
@@ -56,6 +107,9 @@ def generate_task(seed: int, x_up_down, x_scale, y_up_down, y_scale) -> Task:
         unscaled_image = image_create_random_advanced(seed + 1000 + i, min_image_size, computed_x_max_image_size, min_image_size, computed_y_max_image_size)
 
         input_image, output_image = image_scale(unscaled_image, x_up_down, x_scale, y_up_down, y_scale)
+
+        if can_add_noise:
+            input_image = scale_up_and_add_noise(unscaled_image, seed + i * 13392 + 1000, x_scale, y_scale, noise_color)
 
         task.append_pair(input_image, output_image, is_example)
 
@@ -105,7 +159,8 @@ def generate_dataset_item_list(seed: int) -> list[dict]:
     seed_task = seed
 
     max_scale_factor = 7
-    up_down = ['up', 'down']
+    # up_down = ['up', 'down']
+    up_down = ['down']
     config_list = []
     for x_up_down in up_down:
         for y_up_down in up_down:
@@ -137,7 +192,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=51000019,
+    seed=61000019,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
