94882efb406b458bb7ce103805a778be4993f868
Simon Strandgaard <neoneye@gmail.com>
2024-08-27 23:39:45 +0200

    trying newest models

--- simon_arc_model_run/run_tasks.py
+++ simon_arc_model_run/run_tasks.py
@@ -14,10 +14,11 @@ from simon_arc_model.work_manager import WorkManager
 # iteration=255 training=23 evaluation=4 total=27
 # iteration=256 training=22 evaluation=6 total=28
 # iteration=262 training=23 evaluation=5 total=28
-# iteration=289 training=24 evaluation=6 total=30 <-- best 
+# iteration=289 training=24 evaluation=6 total=30 <-- best total
 # iteration=294 training=22 evaluation=6 total=28
-# iteration=309 training=24 evaluation=6 total=30
-model_iteration = 337
+# iteration=309 training=24 evaluation=6 total=30 <-- best total
+# iteration=351 training=20 evaluation=7 total=27 <-- best evaluation
+model_iteration = 353
 model_name = f'simon-arc-lab-model{model_iteration}'
 model_directory = f'/Users/neoneye/nobackup/git/{model_name}'
 
