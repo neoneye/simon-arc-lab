55b95718f8e7791517f603eaa8c3fd115eaeec18
Simon Strandgaard <neoneye@gmail.com>
2024-08-29 20:10:43 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_boundingbox.py
+++ simon_arc_dataset_run/dataset_solve_boundingbox.py
@@ -196,7 +196,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=200000913,
+    seed=210000913,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
