04d3babc834f582135798124a312c374cbaf6df1
Simon Strandgaard <neoneye@gmail.com>
2024-08-31 18:25:39 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_combine.py
+++ simon_arc_dataset_run/dataset_combine.py
@@ -5,7 +5,7 @@ import random
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, PROJECT_ROOT)
 
-seed = 144
+seed = 145
 random.seed(seed)
 
 # Define the input file paths
@@ -35,14 +35,14 @@ file_names = [
     # 'dataset_solve_mask.jsonl',
     # 'dataset_solve_mass.jsonl',
     # 'dataset_solve_outline.jsonl',
-    # 'dataset_solve_probecolor.jsonl',
-    # 'dataset_solve_ray.jsonl',
-    # 'dataset_solve_reverse.jsonl',
-    # 'dataset_solve_rotate.jsonl',
-    'dataset_solve_scale.jsonl',
-    'dataset_solve_symmetry.jsonl',
-    'dataset_solve_translate.jsonl',
-    'dataset_solve_zindex.jsonl',
+    'dataset_solve_probecolor.jsonl',
+    'dataset_solve_ray.jsonl',
+    'dataset_solve_reverse.jsonl',
+    'dataset_solve_rotate.jsonl',
+    # 'dataset_solve_scale.jsonl',
+    # 'dataset_solve_symmetry.jsonl',
+    # 'dataset_solve_translate.jsonl',
+    # 'dataset_solve_zindex.jsonl',
     # 'dataset_symmetry.jsonl',
 ]
 
