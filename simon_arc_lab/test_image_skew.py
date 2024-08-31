51754e21c38b8f6ddbbf53b9b3051beecbc956a4
Simon Strandgaard <neoneye@gmail.com>
2024-08-31 13:47:22 +0200

    image_skew/unskew functions

---
+++ simon_arc_lab/test_image_skew.py
@@ -0,0 +1,63 @@
+import unittest
+import numpy as np
+from .image_skew import *
+
+class TestImageSkew(unittest.TestCase):
+    def test_10000_image_skew_right(self):
+        # Arrange
+        input = np.array([
+            [1, 2, 3],
+            [4, 5, 6]], dtype=np.uint8)
+        # Act
+        actual = image_skew_right(input, 255)
+        # Assert
+        expected = np.array([
+            [1, 2, 3, 255],
+            [255, 4, 5, 6]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_20000_image_unskew_right(self):
+        # Arrange
+        input = np.array([
+            [1, 2, 3, 255],
+            [255, 4, 5, 6]], dtype=np.uint8)
+        # Act
+        actual = image_unskew_right(input)
+        # Assert
+        expected = np.array([
+            [1, 2, 3],
+            [4, 5, 6]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_30000_image_skew_down(self):
+        # Arrange
+        input = np.array([
+            [1, 4],
+            [2, 5],
+            [3, 6]], dtype=np.uint8)
+        # Act
+        actual = image_skew_down(input, 255)
+        # Assert
+        expected = np.array([
+            [1, 255],
+            [2, 4],
+            [3, 5],
+            [255, 6]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_40000_image_unskew_down(self):
+        # Arrange
+        input = np.array([
+            [1, 255],
+            [2, 4],
+            [3, 5],
+            [255, 6]], dtype=np.uint8)
+        # Act
+        actual = image_unskew_down(input)
+        # Assert
+        expected = np.array([
+            [1, 4],
+            [2, 5],
+            [3, 6]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
