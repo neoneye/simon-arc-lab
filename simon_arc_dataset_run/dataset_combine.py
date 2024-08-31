3e9c660c527344cee21532d35aeeaafb89580495
Simon Strandgaard <neoneye@gmail.com>
2024-08-31 23:45:25 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_combine.py
+++ simon_arc_dataset_run/dataset_combine.py
@@ -5,21 +5,21 @@ import random
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, PROJECT_ROOT)
 
-seed = 147
+seed = 148
 random.seed(seed)
 
 # Define the input file paths
 dataset_dir = os.path.dirname(__file__)
 file_names = [
-    # 'dataset_cellular_automaton.jsonl',
-    # 'dataset_dilation.jsonl',
-    # 'dataset_erosion.jsonl',
-    # 'dataset_histogram.jsonl',
-    # 'dataset_image.jsonl',
-    # 'dataset_image_pair.jsonl',
-    # 'dataset_mass.jsonl',
-    # 'dataset_scale.jsonl',
-    # 'dataset_shape.jsonl',
+    'dataset_cellular_automaton.jsonl',
+    'dataset_dilation.jsonl',
+    'dataset_erosion.jsonl',
+    'dataset_histogram.jsonl',
+    'dataset_image.jsonl',
+    'dataset_image_pair.jsonl',
+    'dataset_mass.jsonl',
+    'dataset_scale.jsonl',
+    'dataset_shape.jsonl',
     'dataset_solve_bool.jsonl',
     'dataset_solve_boundingbox.jsonl',
     'dataset_solve_color.jsonl',
@@ -28,26 +28,26 @@ file_names = [
     'dataset_solve_erosion.jsonl',
     'dataset_solve_flip.jsonl',
     'dataset_solve_fractal.jsonl',
-    # 'dataset_solve_gravity.jsonl',
-    # 'dataset_solve_grid.jsonl',
-    # 'dataset_solve_half.jsonl',
-    # 'dataset_solve_halfplane.jsonl',
-    # 'dataset_solve_mask.jsonl',
-    # 'dataset_solve_mass.jsonl',
-    # 'dataset_solve_outline.jsonl',
-    # 'dataset_solve_probecolor.jsonl',
-    # 'dataset_solve_ray.jsonl',
-    # 'dataset_solve_reverse.jsonl',
-    # 'dataset_solve_rotate.jsonl',
-    # 'dataset_solve_scale.jsonl',
-    # 'dataset_solve_symmetry.jsonl',
-    # 'dataset_solve_translate.jsonl',
-    # 'dataset_solve_zindex.jsonl',
-    # 'dataset_symmetry.jsonl',
+    'dataset_solve_gravity.jsonl',
+    'dataset_solve_grid.jsonl',
+    'dataset_solve_half.jsonl',
+    'dataset_solve_halfplane.jsonl',
+    'dataset_solve_mask.jsonl',
+    'dataset_solve_mass.jsonl',
+    'dataset_solve_outline.jsonl',
+    'dataset_solve_probecolor.jsonl',
+    'dataset_solve_ray.jsonl',
+    'dataset_solve_reverse.jsonl',
+    'dataset_solve_rotate.jsonl',
+    'dataset_solve_scale.jsonl',
+    'dataset_solve_symmetry.jsonl',
+    'dataset_solve_translate.jsonl',
+    'dataset_solve_zindex.jsonl',
+    'dataset_symmetry.jsonl',
 ]
 
 # Number of rows in the output file
-output_rows = 20000
+output_rows = 300000
 
 # Calculate the number of rows to sample from each file
 rows_per_file = output_rows // len(file_names)
