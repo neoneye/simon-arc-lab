a0d1c93b301dab1775fb3efa2dcd5b0d7f0e2751
Simon Strandgaard <neoneye@gmail.com>
2024-08-31 14:50:39 +0200

    Testing edge cases

--- simon_arc_lab/test_image_gravity_move.py
+++ simon_arc_lab/test_image_gravity_move.py
@@ -3,7 +3,7 @@ import numpy as np
 from .image_gravity_move import *
 
 class TestImageGravityMove(unittest.TestCase):
-    def test_10000_image_gravity_left(self):
+    def test_10000_image_gravity_move_left(self):
         # Arrange
         input = np.array([
             [5, 0, 0, 0, 0],
@@ -22,7 +22,22 @@ class TestImageGravityMove(unittest.TestCase):
             [7, 7, 0, 0, 0]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
 
-    def test_10001_image_gravity_up(self):
+    def test_10001_image_gravity_move_left(self):
+        # Arrange
+        input = np.array([
+            [1, 1, 2, 2, 3],
+            [0, 0, 4, 4, 4],
+            [5, 0, 5, 0, 5]], dtype=np.uint8)
+        # Act
+        actual = image_gravity_move(input, 0, GravityMoveDirection.LEFT)
+        # Assert
+        expected = np.array([
+            [1, 1, 2, 2, 3],
+            [4, 4, 4, 0, 0],
+            [5, 5, 5, 0, 0]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_20000_image_gravity_move_up(self):
         # Arrange
         input = np.array([
             [5, 0, 0, 0, 0],
@@ -41,7 +56,7 @@ class TestImageGravityMove(unittest.TestCase):
             [0, 0, 0, 0, 0]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
 
-    def test_10002_image_gravity_down(self):
+    def test_30000_image_gravity_move_down(self):
         # Arrange
         input = np.array([
             [5, 0, 0, 0, 0],
@@ -60,7 +75,7 @@ class TestImageGravityMove(unittest.TestCase):
             [5, 7, 7, 4, 5]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
 
-    def test_10003_image_gravity_right(self):
+    def test_40000_image_gravity_move_right(self):
         # Arrange
         input = np.array([
             [5, 0, 0, 0, 0],
