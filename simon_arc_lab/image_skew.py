ec3f4f7b86a305ebbf8c21ef65988e597f621079
Simon Strandgaard <neoneye@gmail.com>
2024-08-31 14:18:45 +0200

    image_skew/unskew_up() added

--- simon_arc_lab/image_skew.py
+++ simon_arc_lab/image_skew.py
@@ -1,5 +1,51 @@
 import numpy as np
 
+def image_skew_up(image: np.array, padding_color: int) -> np.array:
+    """
+    Displace each column up by the column index.
+    """
+    height, width = image.shape
+    skewed_image = np.full((height + width - 1, width), padding_color, dtype=np.uint8)
+    for y in range(height):
+        for x in range(width):
+            skewed_image[width-1-x+y, x] = image[y, x]
+    return skewed_image
+
+def image_unskew_up(image: np.array) -> np.array:
+    """
+    Displace each column down by the column index. Remove the padding.
+    """
+    height, width = image.shape
+    height_unskewed = height - width + 1
+    unskewed_image = np.zeros((height_unskewed, width), dtype=np.uint8)
+    for y in range(height_unskewed):
+        for x in range(width):
+            unskewed_image[y, x] = image[width-1-x+y, x]
+    return unskewed_image
+
+def image_skew_down(image: np.array, padding_color: int) -> np.array:
+    """
+    Displace each column down by the column index.
+    """
+    height, width = image.shape
+    skewed_image = np.full((height + width - 1, width), padding_color, dtype=np.uint8)
+    for y in range(height):
+        for x in range(width):
+            skewed_image[y+x, x] = image[y, x]
+    return skewed_image
+
+def image_unskew_down(image: np.array) -> np.array:
+    """
+    Displace each column up by the column index. Remove the padding.
+    """
+    height, width = image.shape
+    height_unskewed = height - width + 1
+    unskewed_image = np.zeros((height_unskewed, width), dtype=np.uint8)
+    for y in range(height_unskewed):
+        for x in range(width):
+            unskewed_image[y, x] = image[y+x, x]
+    return unskewed_image
+
 def image_skew_left(image: np.array, padding_color: int) -> np.array:
     """
     Displace each row to the left by the row index.
@@ -44,27 +90,4 @@ def image_unskew_right(image: np.array) -> np.array:
     for y in range(height):
         for x in range(width_unskewed):
             unskewed_image[y, x] = image[y, y+x]
-    return unskewed_image
-
-def image_skew_down(image: np.array, padding_color: int) -> np.array:
-    """
-    Displace each column down by the column index.
-    """
-    height, width = image.shape
-    skewed_image = np.full((height + width - 1, width), padding_color, dtype=np.uint8)
-    for y in range(height):
-        for x in range(width):
-            skewed_image[y+x, x] = image[y, x]
-    return skewed_image
-
-def image_unskew_down(image: np.array) -> np.array:
-    """
-    Displace each column up by the column index. Remove the padding.
-    """
-    height, width = image.shape
-    height_unskewed = height - width + 1
-    unskewed_image = np.zeros((height_unskewed, width), dtype=np.uint8)
-    for y in range(height_unskewed):
-        for x in range(width):
-            unskewed_image[y, x] = image[y+x, x]
     return unskewed_image
--- simon_arc_lab/test_image_skew.py
+++ simon_arc_lab/test_image_skew.py
@@ -3,59 +3,39 @@ import numpy as np
 from .image_skew import *
 
 class TestImageSkew(unittest.TestCase):
-    def test_10000_image_skew_left(self):
+    def test_10000_image_skew_up(self):
         # Arrange
         input = np.array([
-            [1, 2, 3],
-            [4, 5, 6]], dtype=np.uint8)
+            [1, 4],
+            [2, 5],
+            [3, 6]], dtype=np.uint8)
         # Act
-        actual = image_skew_left(input, 255)
+        actual = image_skew_up(input, 255)
         # Assert
         expected = np.array([
-            [255, 1, 2, 3],
-            [4, 5, 6, 255]], dtype=np.uint8)
+            [255, 4],
+            [1, 5],
+            [2, 6],
+            [3, 255]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
 
-    def test_20000_image_unskew_left(self):
+    def test_20000_image_unskew_up(self):
         # Arrange
         input = np.array([
-            [255, 1, 2, 3],
-            [4, 5, 6, 255]], dtype=np.uint8)
+            [255, 4],
+            [1, 5],
+            [2, 6],
+            [3, 255]], dtype=np.uint8)
         # Act
-        actual = image_unskew_left(input)
+        actual = image_unskew_up(input)
         # Assert
         expected = np.array([
-            [1, 2, 3],
-            [4, 5, 6]], dtype=np.uint8)
+            [1, 4],
+            [2, 5],
+            [3, 6]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
 
-    def test_30000_image_skew_right(self):
-        # Arrange
-        input = np.array([
-            [1, 2, 3],
-            [4, 5, 6]], dtype=np.uint8)
-        # Act
-        actual = image_skew_right(input, 255)
-        # Assert
-        expected = np.array([
-            [1, 2, 3, 255],
-            [255, 4, 5, 6]], dtype=np.uint8)
-        np.testing.assert_array_equal(actual, expected)
-
-    def test_40000_image_unskew_right(self):
-        # Arrange
-        input = np.array([
-            [1, 2, 3, 255],
-            [255, 4, 5, 6]], dtype=np.uint8)
-        # Act
-        actual = image_unskew_right(input)
-        # Assert
-        expected = np.array([
-            [1, 2, 3],
-            [4, 5, 6]], dtype=np.uint8)
-        np.testing.assert_array_equal(actual, expected)
-
-    def test_50000_image_skew_down(self):
+    def test_30000_image_skew_down(self):
         # Arrange
         input = np.array([
             [1, 4],
@@ -71,7 +51,7 @@ class TestImageSkew(unittest.TestCase):
             [255, 6]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
 
-    def test_60000_image_unskew_down(self):
+    def test_40000_image_unskew_down(self):
         # Arrange
         input = np.array([
             [1, 255],
@@ -87,3 +67,54 @@ class TestImageSkew(unittest.TestCase):
             [3, 6]], dtype=np.uint8)
         np.testing.assert_array_equal(actual, expected)
 
+    def test_50000_image_skew_left(self):
+        # Arrange
+        input = np.array([
+            [1, 2, 3],
+            [4, 5, 6]], dtype=np.uint8)
+        # Act
+        actual = image_skew_left(input, 255)
+        # Assert
+        expected = np.array([
+            [255, 1, 2, 3],
+            [4, 5, 6, 255]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_60000_image_unskew_left(self):
+        # Arrange
+        input = np.array([
+            [255, 1, 2, 3],
+            [4, 5, 6, 255]], dtype=np.uint8)
+        # Act
+        actual = image_unskew_left(input)
+        # Assert
+        expected = np.array([
+            [1, 2, 3],
+            [4, 5, 6]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_70000_image_skew_right(self):
+        # Arrange
+        input = np.array([
+            [1, 2, 3],
+            [4, 5, 6]], dtype=np.uint8)
+        # Act
+        actual = image_skew_right(input, 255)
+        # Assert
+        expected = np.array([
+            [1, 2, 3, 255],
+            [255, 4, 5, 6]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_80000_image_unskew_right(self):
+        # Arrange
+        input = np.array([
+            [1, 2, 3, 255],
+            [255, 4, 5, 6]], dtype=np.uint8)
+        # Act
+        actual = image_unskew_right(input)
+        # Assert
+        expected = np.array([
+            [1, 2, 3],
+            [4, 5, 6]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual, expected)
