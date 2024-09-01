4f47f11b11464e68e1902c8d22af60dd20490237
Simon Strandgaard <neoneye@gmail.com>
2024-09-01 22:07:35 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_gravity.py
+++ simon_arc_dataset_run/dataset_solve_gravity.py
@@ -143,7 +143,7 @@ def generate_task_gravity_draw(seed: int, direction: GravityDrawDirection) -> Ta
     # count_test = 1
     task = Task()
     min_image_size = 3
-    max_image_size = 10
+    max_image_size = 20
     max_number_of_positions = 5
 
     colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -273,7 +273,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=31000194,
+    seed=32000194,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
