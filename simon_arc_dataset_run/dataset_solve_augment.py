7c9d3feb6c9b58b4161db3f95ef94b73a3dd368f
Simon Strandgaard <neoneye@gmail.com>
2024-09-03 09:55:39 +0200

    Save the generated tasks to a file

--- simon_arc_dataset_run/dataset_solve_augment.py
+++ simon_arc_dataset_run/dataset_solve_augment.py
@@ -148,181 +148,6 @@ class NodeSwapColors(BaseNode):
     def name(self) -> str:
         return 'swap_colors'
 
-def generate_task_augmented(seed: int) -> Task:
-    """
-    Draw 2 crossing lines on an image.
-    Highlight the lines that have a particular direction.
-    Highlight the intersection of the 2 lines.
-    """
-    count_example = random.Random(seed + 1).randint(3, 4)
-    count_test = random.Random(seed + 2).randint(1, 2)
-    # count_test = 1
-    task = Task()
-    min_image_size = 3
-    max_image_size = 10
-
-    input_colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    random.Random(seed + 3).shuffle(input_colors)
-    color_map_input = {}
-    for i, color in enumerate(input_colors):
-        color_map_input[i] = color
-
-    output_colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    random.Random(seed + 4).shuffle(output_colors)
-    color_map_output = {}
-    for i, color in enumerate(output_colors):
-        color_map_output[i] = color
-
-    available_line_ids = ['left_right', 'top_bottom', 'topleft_bottomright', 'topright_bottomleft']
-    # pick two unique elements
-    line_ids = random.Random(seed + 5).sample(available_line_ids, 2)
-
-    # pick one of the line_ids as the output, or take the intersection
-    available_output_ids = ['intersection'] + line_ids
-    output_id = random.Random(seed + 6).choice(available_output_ids)
-
-    available_input_ids = ['mask', 'color']
-    input_id = random.Random(seed + 7).choice(available_input_ids)
-
-    intersection_variant = random.Random(seed + 8).randint(0, 2)
-    hide_intersection_point = intersection_variant == 1
-    use_different_color_for_intersection_point = intersection_variant == 2
-
-    pretty_line_ids = '_'.join(line_ids)
-    pretty_line_ids = pretty_line_ids.replace('left_right', 'lr')
-    pretty_line_ids = pretty_line_ids.replace('top_bottom', 'tb')
-    pretty_line_ids = pretty_line_ids.replace('topleft_bottomright', 'tlbr')
-    pretty_line_ids = pretty_line_ids.replace('topright_bottomleft', 'trbl')
-    task.metadata_task_id = f'cross {pretty_line_ids} {input_id}{intersection_variant} {output_id}'
-
-    has_diagonal_lines = 'topleft_bottomright' in line_ids or 'topright_bottomleft' in line_ids
-    # print(f"has_diagonal_lines: {has_diagonal_lines}")
-
-    for i in range(count_example+count_test):
-        is_example = i < count_example
-        is_test = i >= count_example
-        input_image = None
-        output_image = None
-        for retry_index in range(100):
-            iteration_seed = (retry_index * 10000) + (seed * 37) + (i * 9932342) + 101
-
-            width = random.Random(iteration_seed + 1).randint(min_image_size, max_image_size)
-            height = random.Random(iteration_seed + 2).randint(min_image_size, max_image_size)
-            max_size = max(width, height)
-
-            # This is where the 2 lines are going to intersect
-            x = random.Random(iteration_seed + 7).randint(0, width-1)
-            y = random.Random(iteration_seed + 8).randint(0, height-1)
-
-            # It's not possible to determine where 2 lines intersect when it's on a corner. 
-            # Skip the corners, when it's a test pair.
-            # Allow corners for examples, so there is some ambiguity.
-            is_x0 = x == 0
-            is_x1 = x == width-1
-            is_y0 = y == 0
-            is_y1 = y == height-1
-            is_x0y0 = is_x0 and is_y0
-            is_x1y0 = is_x1 and is_y0
-            is_x0y1 = is_x0 and is_y1
-            is_x1y1 = is_x1 and is_y1
-            position_in_corner = is_x0y0 or is_x1y0 or is_x0y1 or is_x1y1
-            if is_test and has_diagonal_lines and position_in_corner:
-                # print("Skip corner")
-                continue
-
-            accumulated_image_or = image_create(width, height, 0)
-            accumulated_image_xor = image_create(width, height, 0)
-            accumulated_image_intersection = image_create(width, height, 0)
-            accumulated_image_lr = image_create(width, height, 0)
-            accumulated_image_tb = image_create(width, height, 0)
-            accumulated_image_tlbr = image_create(width, height, 0)
-            accumulated_image_trbl = image_create(width, height, 0)
-
-            drawing_image = image_create(width, height, 0)
-
-            # Draw the 2 lines
-            for j, line_id in enumerate(line_ids):
-                image = image_create(width, height, 0)
-                draw_color = j + 1
-
-                if line_id == 'left_right':
-                    image = image_bresenham_line(image, 0, y, width-1, y, 1)
-                    drawing_image = image_bresenham_line(drawing_image, 0, y, width-1, y, draw_color)
-                    accumulated_image_lr = image_mask_or(accumulated_image_lr, image)
-                elif line_id == 'top_bottom':
-                    image = image_bresenham_line(image, x, 0, x, height-1, 1)
-                    drawing_image = image_bresenham_line(drawing_image, x, 0, x, height-1, draw_color)
-                    accumulated_image_tb = image_mask_or(accumulated_image_tb, image)
-                elif line_id == 'topleft_bottomright':
-                    image = image_bresenham_line(image, x - max_size, y - max_size, x + max_size, y + max_size, 1)
-                    drawing_image = image_bresenham_line(drawing_image, x - max_size, y - max_size, x + max_size, y + max_size, draw_color)
-                    accumulated_image_tlbr = image_mask_or(accumulated_image_tlbr, image)
-                elif line_id == 'topright_bottomleft':
-                    image = image_bresenham_line(image, x + max_size, y - max_size, x - max_size, y + max_size, 1)
-                    drawing_image = image_bresenham_line(drawing_image, x + max_size, y - max_size, x - max_size, y + max_size, draw_color)
-                    accumulated_image_trbl = image_mask_or(accumulated_image_trbl, image)
-
-                intersection_mask = image_mask_and(accumulated_image_or, image)
-                accumulated_image_or = image_mask_or(accumulated_image_or, image)
-                accumulated_image_xor = image_mask_xor(accumulated_image_xor, image)
-                accumulated_image_intersection = image_mask_or(accumulated_image_intersection, intersection_mask)
-
-            if hide_intersection_point:
-                drawing_image[y, x] = 0
-            elif use_different_color_for_intersection_point:
-                drawing_image[y, x] = 3
-
-            # Prepare input image
-            input_image_raw = None
-            if input_id == 'mask':
-                input_image_raw = accumulated_image_xor
-            elif input_id == 'color':
-                input_image_raw = drawing_image
-            input_image = image_replace_colors(input_image_raw, color_map_input)
-
-            # We are not interested in an empty image
-            histogram_input = Histogram.create_with_image(input_image)
-            if histogram_input.number_of_unique_colors() < 2:
-                continue
-
-            # Prepare output image
-            output_image_raw = None
-            if output_id == 'left_right':
-                output_image_raw = accumulated_image_lr
-            elif output_id == 'top_bottom':
-                output_image_raw = accumulated_image_tb
-            elif output_id == 'topleft_bottomright':
-                output_image_raw = accumulated_image_tlbr
-            elif output_id == 'topright_bottomleft':
-                output_image_raw = accumulated_image_trbl
-            elif output_id == 'intersection':
-                output_image_raw = accumulated_image_intersection
-            output_image = image_replace_colors(output_image_raw, color_map_output)
-
-            # We are not interested in an empty image
-            histogram_output = Histogram.create_with_image(output_image)
-            if histogram_output.number_of_unique_colors() < 2:
-                continue
-
-            break
-        if (input_image is None) or (output_image is None):
-            raise Exception("Failed to find a candidate images.")
-        task.append_pair(input_image, output_image, is_example)
-
-    return task
-
-def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
-    builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
-    builder.append_image()
-    return builder.dataset_items()
-
-def generate_dataset_item_list(seed: int) -> list[dict]:
-    transformation_id = 'cross'
-    task = generate_task_augmented(seed)
-    # task.show()
-    dataset_items = generate_dataset_item_list_inner(seed, task, transformation_id)
-    return dataset_items
-
 path_to_arc_dataset_collection_dataset = '/Users/neoneye/git/arc-dataset-collection/dataset'
 if not os.path.isdir(path_to_arc_dataset_collection_dataset):
     print(f"ARC dataset collection directory '{path_to_arc_dataset_collection_dataset}' does not exist.")
@@ -418,7 +243,8 @@ for group_index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotask
     node_swap_colors = NodeSwapColors()
     node_rotate = NodeRotateCW()
     node_scale = NodeScale('up', 2, 'up', 2)
-    node_transform = NodeChain([node_swap_colors, node_rotate, node_scale])
+    # node_transform = NodeChain([node_swap_colors, node_rotate, node_scale])
+    node_transform = NodeChain([node_rotate])
 
     for task_index, task in enumerate(taskset.tasks):
         iteration_seed = group_index * 1000000 + task_index * 1000
@@ -427,18 +253,34 @@ for group_index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotask
         new_tasks = create_augmented_tasks('output', node_pre, node_transform, iteration_seed + 2, task)
         augmented_tasks.extend(new_tasks)
 
-print(f"Number of augmented tasks: {len(augmented_tasks)}")
+count_augmented_tasks = len(augmented_tasks)
+print(f"Number of augmented tasks: {count_augmented_tasks}")
 
-for task in augmented_tasks:
-    task.show()
+# for task in augmented_tasks:
+#     task.show()
 
-# generator = DatasetGenerator(
-#     generate_dataset_item_list_fn=generate_dataset_item_list
-# )
-# generator.generate(
-#     seed=1200023425,
-#     max_num_samples=100000,
-#     max_byte_size=1024*1024*100
-# )
-# # generator.inspect()
-# generator.save(SAVE_FILE_PATH)
+def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
+    builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
+    builder.append_image()
+    return builder.dataset_items()
+
+def generate_dataset_item_list(seed: int) -> list[dict]:
+    task = augmented_tasks[seed % count_augmented_tasks]
+    transformation_id = task.metadata_task_id
+    # task.show()
+    dataset_items = generate_dataset_item_list_inner(seed, task, transformation_id)
+    return dataset_items
+
+max_num_samples = min(1000, count_augmented_tasks)
+print(f"max_num_samples: {max_num_samples}")
+
+generator = DatasetGenerator(
+    generate_dataset_item_list_fn=generate_dataset_item_list
+)
+generator.generate(
+    seed=1200023425,
+    max_num_samples=max_num_samples,
+    max_byte_size=1024*1024*100
+)
+# generator.inspect()
+generator.save(SAVE_FILE_PATH)
