bd18233195f764f233fcb39e3ba0cf3242861854
Simon Strandgaard <neoneye@gmail.com>
2024-09-03 20:19:55 +0200

    Experimenting with context length 1024 (takes much longer time to train).

--- simon_arc_model/model.py
+++ simon_arc_model/model.py
@@ -2,10 +2,11 @@ from transformers import T5ForConditionalGeneration, RobertaTokenizer
 
 class Model:
     def __init__(self, pretrained_model_name_or_path: str, input_max_length: int):
-        good_input_max_length = input_max_length == 256 or input_max_length == 512
+        good_input_max_length = input_max_length == 256 or input_max_length == 512 or input_max_length == 1024
         if not good_input_max_length:
             # As of 2024-august-09, the model has been trained on both input_max_length 256 and 512.
-            raise ValueError("input_max_length must be 256 or 512")
+            # As of 2024-september-03, the model has been trained on both input_max_length 1024.
+            raise ValueError("input_max_length must be 256 or 512 or 1024")
         
         self.model = T5ForConditionalGeneration.from_pretrained(pretrained_model_name_or_path)
         self.tokenizer = RobertaTokenizer.from_pretrained(pretrained_model_name_or_path)
