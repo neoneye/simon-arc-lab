22bc30561ab442858a7f6fed5df7b40012f426c9
Simon Strandgaard <neoneye@gmail.com>
2024-09-03 20:53:13 +0200

    Easier to switch between 512 and 1024.

--- simon_arc_model_run/run_tasks.py
+++ simon_arc_model_run/run_tasks.py
@@ -9,6 +9,10 @@ from simon_arc_lab.gallery_generator import gallery_generator_run
 from simon_arc_model.model import Model
 from simon_arc_model.work_manager import WorkManager
 
+# A power of 2 value, and the max length of the input prompt
+CONTEXT_SIZE_LIMIT = (512, 500)
+# CONTEXT_SIZE_LIMIT = (1024, 1000)
+
 # iteration=229 training=21 evaluation=6 total=27
 # iteration=240 training=23 evaluation=4 total=27
 # iteration=254 training=23 evaluation=4 total=27
@@ -47,8 +51,11 @@ for groupname, path_to_task_dir in groupname_pathtotaskdir_list:
         print(f"path_to_task_dir directory '{path_to_task_dir}' does not exist.")
         sys.exit(1)
 
+context_length, max_prompt_length = CONTEXT_SIZE_LIMIT
+print(f"context length: {context_length}, max prompt length: {max_prompt_length}")
+
 # Load model
-model = Model(model_directory, 512)
+model = Model(model_directory, context_length)
 
 number_of_items_in_list = len(groupname_pathtotaskdir_list)
 for index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotaskdir_list):
@@ -58,7 +65,7 @@ for index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotaskdir_li
     taskset = TaskSet.load_directory(path_to_task_dir)
 
     wm = WorkManager(model, taskset)
-    wm.discard_items_with_too_long_prompts(500)
+    wm.discard_items_with_too_long_prompts(max_prompt_length)
     # wm.process_all_work_items()
     wm.process_all_work_items(save_dir=save_dir)
     # wm.process_all_work_items(show=True)
