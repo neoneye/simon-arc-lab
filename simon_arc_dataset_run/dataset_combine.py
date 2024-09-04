6eaffd9f90da96ebd8cf93b5afa30321acfdc536
Simon Strandgaard <neoneye@gmail.com>
2024-09-04 10:31:39 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_combine.py
+++ simon_arc_dataset_run/dataset_combine.py
@@ -6,7 +6,7 @@ import json
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, PROJECT_ROOT)
 
-seed = 158
+seed = 160
 random.seed(seed)
 
 # Define the input file paths
--- simon_arc_dataset_run/dataset_solve_cross.py
+++ simon_arc_dataset_run/dataset_solve_cross.py
@@ -212,7 +212,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=1200023425,
+    seed=1300023425,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
