ddf0da412e30c7ae6e501e67777cb6ad8dd9065f
Simon Strandgaard <neoneye@gmail.com>
2024-09-05 12:01:47 +0200

    Experiments with random patterns intersecting

--- simon_arc_dataset_run/dataset_solve_span.py
+++ simon_arc_dataset_run/dataset_solve_span.py
@@ -18,6 +18,7 @@ from simon_arc_lab.image_symmetry import *
 from simon_arc_lab.histogram import Histogram
 from simon_arc_lab.benchmark import *
 from simon_arc_lab.image_rect import image_rect
+from simon_arc_lab.image_scale import image_scale_up_variable
 from simon_arc_lab.rectangle import Rectangle
 from simon_arc_dataset.simon_solve_version1_names import SIMON_SOLVE_VERSION1_NAMES
 from simon_arc_dataset.generate_solve import *
@@ -178,13 +179,178 @@ def generate_task_with_intersecting_spans(seed: int, transformation_id: str) -> 
 
     return task
 
+def generate_task_with_intersecting_spans2(seed: int, transformation_id: str) -> Task:
+    """
+    Spans that are intersecting.
+
+    Examples:
+    https://neoneye.github.io/arc/edit.html?dataset=ARC&task=a406ac07
+    https://neoneye.github.io/arc/edit.html?dataset=ARC&task=2281f1f4
+    https://neoneye.github.io/arc/edit.html?dataset=ARC&task=c7d4e6ad
+    https://neoneye.github.io/arc/edit.html?dataset=ARC&task=c9f8e694
+    """
+    count_example = random.Random(seed + 1).randint(2, 3)
+    count_test = random.Random(seed + 2).randint(1, 2)
+    # count_test = 1
+    task = Task()
+    min_span_count = 3
+    max_span_count = 7
+    max_image_size = 12
+
+    color_background = 9
+    color_template = 8
+
+    span_color_count = random.Random(seed + 3).randint(2, 6)
+
+    colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    random.Random(seed + 5).shuffle(colors)
+    color_mapping = {}
+    for i in range(10):
+        color_mapping[i] = colors[i]
+
+    task.metadata_task_id = f'intersecting_spans_random {transformation_id}'
+
+    # Rotate the image, so the tile is not always at the top-left, but also in the other corners.
+    rotate90_count = random.Random(seed + 3).randint(0, 3)
+
+    for i in range(count_example+count_test):
+        is_example = i < count_example
+        output_image = None
+        input_image = None
+        for retry_index in range(100):
+            iteration_seed = seed * 5 + retry_index * 133 + i * 1000
+
+            span_count = random.Random(iteration_seed + 1).randint(min_span_count, max_span_count)
+
+            image_width = random.Random(iteration_seed + 2).randint(span_count, max_image_size)
+            image_height = random.Random(iteration_seed + 3).randint(span_count, max_image_size)
+
+            ratios = [0.1, 0.2, 0.3, 0.4, 0.5]
+            ratio = random.Random(iteration_seed + 11).choice(ratios)
+            random_image = image_create_random_with_two_colors(image_width, image_height, color_background, color_template, ratio, iteration_seed + 12)
+
+            x_span_list = [1] * span_count
+            for i in range(image_width - span_count):
+                item_index = random.Random(iteration_seed + 4 + i * 10000).randint(0, span_count - 1)
+                x_span_list[item_index] += 1
+
+            y_span_list = [1] * span_count
+            for i in range(image_height - span_count):
+                item_index = random.Random(iteration_seed + 5 + i * 383838).randint(0, span_count - 1)
+                y_span_list[item_index] += 1
+
+            span_color_list = []
+            last_color = None
+            for i in range(span_count):
+                color = random.Random(iteration_seed + 6 + i).randint(0, span_color_count - 1)
+                if color == last_color:
+                    color = (last_color + 1) % span_color_count
+                last_color = color
+                span_color_list.append(color)
+
+            # Primary area
+            primary_empty = image_create(image_width, image_height, color_background)
+            primary_color_image = image_create(image_width, image_height, color_background)
+            primary_mask = image_create(image_width, image_height, color_background)
+            current_y = 0
+            current_x = 0
+            for span_index in range(span_count):
+                height = y_span_list[span_index]
+                width = x_span_list[span_index]
+                y = current_y
+                current_y += height
+                x = current_x
+                current_x += width
+                color = span_color_list[span_index]
+                primary_color_image = image_rect(primary_color_image, Rectangle(x, y, image_width - x, image_height - y), color)
+
+            dest = image_create(span_count, span_count, 0)
+            current_y = 0
+            current_x = 0
+            for span_index in range(span_count):
+                height = y_span_list[span_index]
+                width = x_span_list[span_index]
+                y = current_y
+                x = current_x
+                current_y += 1
+                current_x += 1
+                color = span_color_list[span_index]
+                dest = image_rect(dest, Rectangle(x, y, span_count - x, span_count - y), color)
+
+            primary_color_image = image_scale_up_variable(dest, x_span_list, y_span_list)
+
+            primary_mask = random_image
+            primary_mask = primary_color_image
+
+            # Vertical border
+            vertical_image = image_create(1, image_height, color_background)
+            current_y = 0
+            for span_y in range(span_count):
+                color = span_color_list[span_y]
+                height = y_span_list[span_y]
+                y = current_y
+                current_y += height
+                vertical_image = image_rect(vertical_image, Rectangle(0, y, 1, height), color)
+
+            # Horizontal border
+            horizontal_image = image_create(image_width, 1, color_background)
+            current_x = 0
+            for span_x in range(span_count):
+                color = span_color_list[span_x]
+                width = x_span_list[span_x]
+                x = current_x
+                current_x += width
+                horizontal_image = image_rect(horizontal_image, Rectangle(x, 0, width, 1), color)
+
+            color_of_last_span = span_color_list[-1]
+            bottom_right_image = image_create(1, 1, color_of_last_span)
+
+            if transformation_id == 'empty_primary_area':
+                input_image_raw = grid2x2(primary_empty, vertical_image, horizontal_image, bottom_right_image)
+                output_image_raw = grid2x2(primary_color_image, vertical_image, horizontal_image, bottom_right_image)
+            elif transformation_id == 'template_primary_area':
+                input_image_raw = grid2x2(primary_mask, vertical_image, horizontal_image, bottom_right_image)
+                output_image_raw = grid2x2(primary_color_image, vertical_image, horizontal_image, bottom_right_image)
+            elif transformation_id == 'template_primary_area_without_border':
+                input_image_raw = grid2x2(primary_mask, vertical_image, horizontal_image, bottom_right_image)
+                output_image_raw = primary_color_image
+            elif transformation_id == 'colored_primary_area_fill_template_border':
+                input_image_raw = primary_color_image
+                output_image_raw = grid2x2(primary_color_image, vertical_image, horizontal_image, bottom_right_image)
+            elif transformation_id == 'colored_primary_area_extract_horizontal':
+                input_image_raw = primary_color_image
+                output_image_raw = horizontal_image
+            elif transformation_id == 'colored_primary_area_extract_vertical':
+                input_image_raw = primary_color_image
+                output_image_raw = vertical_image
+            else:
+                raise Exception(f"Unknown transformation_id: {transformation_id}")
+
+            # Rotate
+            input_image_raw = np.rot90(input_image_raw, rotate90_count)
+            output_image_raw = np.rot90(output_image_raw, rotate90_count)
+
+            # Palette
+            input_image = image_replace_colors(input_image_raw, color_mapping)
+            output_image = image_replace_colors(output_image_raw, color_mapping)
+
+            break
+        if input_image is None:
+            raise Exception("Failed to create image")
+        if output_image is None:
+            raise Exception("Failed to create image")
+        task.append_pair(input_image, output_image, is_example)
+
+    return task
+
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
     builder.append_image()
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
-    j = seed % 5
+    j = seed % 6
+    j = 5
     if j == 0:
         task = generate_task_with_intersecting_spans(seed, 'empty_primary_area')
         transformation_id = 'intersecting_spans empty_primary_area'
@@ -204,7 +370,10 @@ def generate_dataset_item_list(seed: int) -> list[dict]:
     #     # This is identical to 'colored_primary_area_extract_horizontal', due to rotation of the tasks.
     #     task = generate_task_with_intersecting_spans(seed, 'colored_primary_area_extract_vertical')
     #     transformation_id = 'intersecting_spans colored_primary_area_extract_vertical'
-    # task.show()
+    elif j == 5:
+        task = generate_task_with_intersecting_spans2(seed, 'template_primary_area')
+        transformation_id = 'intersecting_spans_random'
+    task.show()
     dataset_items = generate_dataset_item_list_inner(seed, task, transformation_id)
     return dataset_items
 
