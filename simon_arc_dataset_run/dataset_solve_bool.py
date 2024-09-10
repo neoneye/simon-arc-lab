df3e6e3cb9b07a8c6f49840b6bc0db3044da7728
Simon Strandgaard <neoneye@gmail.com>
2024-09-11 00:52:58 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_bool.py
+++ simon_arc_dataset_run/dataset_solve_bool.py
@@ -174,7 +174,7 @@ def generate_task_bool_transformation(seed: int, transformation_id: str) -> Task
 
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
-    builder.append_image()
+    builder.append_image_randomized()
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
@@ -195,7 +195,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=220000771,
+    seed=230000771,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
--- simon_arc_dataset_run/dataset_solve_boundingbox.py
+++ simon_arc_dataset_run/dataset_solve_boundingbox.py
@@ -167,7 +167,7 @@ def generate_task_boundingbox_of_lonely_pixels(seed: int, transformation_id: str
 
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
-    builder.append_image()
+    builder.append_image_randomized()
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
@@ -198,7 +198,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=210000913,
+    seed=220000913,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
