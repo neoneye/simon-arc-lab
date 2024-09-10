f0875863f5e33b17d1fbb3c517830773a600fd30
Simon Strandgaard <neoneye@gmail.com>
2024-09-10 17:19:28 +0200

    Revert "Experiments using mps instead of cpu"
    This reverts commit cfacf1b8600e27c49f2ad5e2828c4f7fef746f5a.

--- simon_arc_model/model.py
+++ simon_arc_model/model.py
@@ -1,47 +1,25 @@
-import torch
 from transformers import T5ForConditionalGeneration, RobertaTokenizer
 
 class Model:
     def __init__(self, pretrained_model_name_or_path: str, input_max_length: int):
-        good_input_max_length = input_max_length in {256, 512, 1024}
+        good_input_max_length = input_max_length == 256 or input_max_length == 512 or input_max_length == 1024
         if not good_input_max_length:
-            raise ValueError("input_max_length must be 256, 512, or 1024")
-
+            # As of 2024-august-09, the model has been trained on both input_max_length 256 and 512.
+            # As of 2024-september-03, the model has been trained on both input_max_length 1024.
+            raise ValueError("input_max_length must be 256 or 512 or 1024")
+        
         self.model = T5ForConditionalGeneration.from_pretrained(pretrained_model_name_or_path)
         self.tokenizer = RobertaTokenizer.from_pretrained(pretrained_model_name_or_path)
         self.input_max_length = input_max_length
-
-        # Determine the device: TPU, CUDA, MPS, or CPU
-        # if 'KAGGLE_URL_BASE' in os.environ:  # Check if running in Kaggle
-        #     try:
-        #         import torch_xla.core.xla_model as xm
-        #         self.device = xm.xla_device()
-        #     except ImportError:
-        #         raise EnvironmentError("torch_xla is required for TPU in Kaggle.")
-        if torch.cuda.is_available():
-            print("Model is using CUDA")
-            self.device = torch.device("cuda")
-        elif torch.backends.mps.is_available():
-            print("Model is using MPS")
-            self.device = torch.device("mps")
-        else:
-            print("Model is using CPU")
-            self.device = torch.device("cpu")
-
-        # Move the model to the appropriate device
-        self.model.to(self.device)
-
+    
     def process(self, prompt: str) -> str:
-        inputs = self.tokenizer(
+        input_ids = self.tokenizer(
             prompt, 
             return_tensors='pt',
             max_length=self.input_max_length,
             padding='max_length',
             truncation=True
-        )
-
-        input_ids = inputs.input_ids.to(self.device)
-        attention_mask = inputs.attention_mask.to(self.device)  # Pass the attention mask
+        ).input_ids
 
         # Tweaking these parameters, may yield better results:
         # num_beams=3,
@@ -49,7 +27,6 @@ class Model:
         # temperature=0.7,
         outputs = self.model.generate(
             input_ids,
-            attention_mask=attention_mask,  # Include the attention mask here
             max_length=128,
             num_beams=5,
             early_stopping=True
