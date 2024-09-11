b7145a278f1e54c39ebe92325ad924eca6cb3d16
Simon Strandgaard <neoneye@gmail.com>
2024-09-11 12:23:49 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_cross.py
+++ simon_arc_dataset_run/dataset_solve_cross.py
@@ -198,7 +198,7 @@ def generate_task_two_crossing_lines(seed: int) -> Task:
 
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
-    builder.append_image()
+    builder.append_image_randomized()
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
@@ -212,7 +212,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=1300023425,
+    seed=1400023425,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
