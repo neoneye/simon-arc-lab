5568b6ccbab32821c356759bcdce86a0208c1689
Simon Strandgaard <neoneye@gmail.com>
2024-09-11 19:38:41 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_half.py
+++ simon_arc_dataset_run/dataset_solve_half.py
@@ -109,7 +109,7 @@ def generate_task_half(seed: int, edge_name: str, connectivity: PixelConnectivit
 
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
-    builder.append_image()
+    builder.append_image_randomized()
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
@@ -141,7 +141,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=1309377377,
+    seed=1319377377,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
