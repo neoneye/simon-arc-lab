497e13cbb536cbf83a477ed7c1630ec8be2bdefc
Simon Strandgaard <neoneye@gmail.com>
2024-09-12 13:15:34 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_probecolor.py
+++ simon_arc_dataset_run/dataset_solve_probecolor.py
@@ -85,7 +85,7 @@ def generate_task(seed: int) -> Task:
 
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
-    builder.append_image()
+    builder.append_image_randomized()
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
@@ -99,7 +99,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=404300232,
+    seed=414300232,
     max_num_samples=100000,
     max_byte_size=1024*1024*100
 )
