649942dc2ea07a4addc9f7a184a7b8850ca6390b
Simon Strandgaard <neoneye@gmail.com>
2024-09-14 02:26:19 +0200

    prefix without tailing underscore

--- simon_arc_dataset_run/export_json_files_from_big_jsonl_file.py
+++ simon_arc_dataset_run/export_json_files_from_big_jsonl_file.py
@@ -2,10 +2,10 @@ import json
 
 MAX_FILE_COUNT = 100
 
-FILENAME_PREFIX = 'symmetry_'
+FILENAME_PREFIX = 'alternating_intersection_sum'
 
 # File paths
-input_file = 'simon_arc_dataset_run/dataset_solve_symmetry.jsonl'
+input_file = 'simon_arc_dataset_run/dataset_solve_span.jsonl'
 output_folder = 'simon_arc_dataset_run/output_folder/'
 
 # Read the JSONL file and export each row as a separate JSON file
@@ -19,7 +19,7 @@ with open(input_file, 'r') as f:
         json_data = json.loads(line)
         
         # Define the output file name based on the row number
-        output_file = f"{output_folder}{FILENAME_PREFIX}{index}.json"
+        output_file = f"{output_folder}{FILENAME_PREFIX}_{index}.json"
         
         # Write the JSON object to a new file
         with open(output_file, 'w') as outfile:
