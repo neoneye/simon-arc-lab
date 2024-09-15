3c9665802f6b7e67d8df61c3ee989c60bc46b034
Simon Strandgaard <neoneye@gmail.com>
2024-09-15 23:13:58 +0200

    brainstorming

---
+++ simon_arc_lab/image_similarity.py
@@ -0,0 +1,9 @@
+# width, height
+# identical histogram
+# same total pixel count
+# same number of unique colors
+# same count of a particular color
+# sort histogram of colors, compare counters
+# same orientation: portrait, landscape, square
+# bigrams
+# trigrams
