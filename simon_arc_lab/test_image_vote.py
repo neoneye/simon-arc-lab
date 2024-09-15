da1cdcd20145d98c6e1171c672890adde5e29042
Simon Strandgaard <neoneye@gmail.com>
2024-09-16 01:34:42 +0200

    image_vote() deal with ambiguous data

--- simon_arc_lab/test_image_vote.py
+++ simon_arc_lab/test_image_vote.py
@@ -1,21 +1,6 @@
 import unittest
 import numpy as np
-
-def image_vote(images: list[np.array]) -> np.array:
-    image0 = images[0]
-    height, width = image0.shape
-    vote = np.zeros((height, width), dtype=np.uint8)
-    for y in range(height):
-        for x in range(width):
-            counts = {}
-            for image in images:
-                pixel = image[y, x]
-                if pixel in counts:
-                    counts[pixel] += 1
-                else:
-                    counts[pixel] = 1
-            vote[y, x] = max(counts, key=counts.get)
-    return vote
+from .image_vote import *
 
 class TestImageVote(unittest.TestCase):
     def test_vote_unambiguous(self):
@@ -35,6 +20,35 @@ class TestImageVote(unittest.TestCase):
             [2, 2, 1],
             [2, 2, 2]], dtype=np.uint8)
         images = [image0, image1, image2]
+        actual = image_vote(images)
+        expected = np.array([
+            [1, 1, 1], 
+            [1, 1, 1], 
+            [2, 2, 2],
+            [2, 2, 2]], dtype=np.uint8)
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_vote_ambiguous_sizes(self):
+        image0 = np.array([
+            [1, 1, 5], 
+            [2, 2, 2],
+            [2, 2, 2]], dtype=np.uint8)
+        image1 = np.array([
+            [1, 1, 5], 
+            [1, 1, 1], 
+            [2, 2, 2],
+            [2, 2, 2]], dtype=np.uint8)
+        image2 = np.array([
+            [3, 1, 1], 
+            [1, 1, 1], 
+            [2, 5, 2],
+            [2, 2, 2]], dtype=np.uint8)
+        image3 = np.array([
+            [1, 1, 1], 
+            [1, 1, 2], 
+            [2, 2, 1],
+            [2, 2, 2]], dtype=np.uint8)
+        images = [image0, image1, image2, image3]
         actual = image_vote(images)
         expected = np.array([
             [1, 1, 1], 
