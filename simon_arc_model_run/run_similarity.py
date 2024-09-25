b5783fb7328f2f67b66ea1f05d104a84a040d60b
Simon Strandgaard <neoneye@gmail.com>
2024-09-25 23:11:10 +0200

    summary of the measured similarity

--- simon_arc_model_run/run_similarity.py
+++ simon_arc_model_run/run_similarity.py
@@ -16,11 +16,13 @@ if not os.path.isdir(path_to_arc_dataset_collection_dataset):
     sys.exit(1)
 
 groupname_pathtotaskdir_list = [
-    # ('arcagi_training', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/training')),
-    # ('arcagi_evaluation', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/evaluation')),
+    ('arcagi_training', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/training')),
+    ('arcagi_evaluation', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/evaluation')),
     ('tama', os.path.join(path_to_arc_dataset_collection_dataset, 'arc-dataset-tama/data')),
-    # ('miniarc', os.path.join(path_to_arc_dataset_collection_dataset, 'Mini-ARC/data')),
-    # ('conceptarc', os.path.join(path_to_arc_dataset_collection_dataset, 'ConceptARC/data')),
+    ('miniarc', os.path.join(path_to_arc_dataset_collection_dataset, 'Mini-ARC/data')),
+    ('conceptarc', os.path.join(path_to_arc_dataset_collection_dataset, 'ConceptARC/data')),
+    ('rearc_easy', os.path.join(path_to_arc_dataset_collection_dataset, 'RE-ARC/data/easy')),
+    ('rearc_hard', os.path.join(path_to_arc_dataset_collection_dataset, 'RE-ARC/data/hard')),
     # ('testdata', os.path.join(PROJECT_ROOT, 'testdata', 'simple_arc_tasks')),
 ]
 
@@ -29,12 +31,14 @@ for groupname, path_to_task_dir in groupname_pathtotaskdir_list:
         print(f"path_to_task_dir directory '{path_to_task_dir}' does not exist.")
         sys.exit(1)
 
+summary_list = []
 number_of_items_in_list = len(groupname_pathtotaskdir_list)
 for index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotaskdir_list):
     print(f"Processing {index+1} of {number_of_items_in_list}. Group name '{groupname}'")
 
     taskset = TaskSet.load_directory(path_to_task_dir)
 
+    accumulated_score_average_list = []
     for task in taskset.tasks:
         score_list = []
         for i in range(task.count_examples + task.count_tests):
@@ -47,4 +51,11 @@ for index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotaskdir_li
         score_max = max(score_list)
         score_average = sum(score_list) / len(score_list)
         score_std_dev = np.std(score_list, ddof=1)
-        print(f"Task: '{task.metadata_task_id}'    min: {score_min} average: {score_average:,.1f} max: {score_max} std_dev: {score_std_dev:,.1f}")
+        # print(f"Task: '{task.metadata_task_id}'    min: {score_min} average: {score_average:,.1f} max: {score_max} std_dev: {score_std_dev:,.1f}")
+        accumulated_score_average_list.append(score_average)
+
+    accumulated_score_average = sum(accumulated_score_average_list) / len(accumulated_score_average_list)
+    summary = f"Group: '{groupname}'    similarity: {accumulated_score_average:,.1f}"
+    summary_list.append(summary)
+
+print("Summary:\n" + "\n".join(summary_list))
