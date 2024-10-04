b9ed5e5a7b255b7734d8269a7555d39ca602ec5f
Simon Strandgaard <neoneye@gmail.com>
2024-10-04 15:39:19 +0200

    generated dataset

--- simon_arc_dataset_run/dataset_solve_probecolor.py
+++ simon_arc_dataset_run/dataset_solve_probecolor.py
@@ -99,8 +99,8 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=414300232,
-    max_num_samples=100000,
+    seed=414400232,
+    max_num_samples=1000,
     max_byte_size=1024*1024*100
 )
 # generator.inspect()
