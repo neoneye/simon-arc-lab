0eb4f4af4490f8ab552d92985ea540385c9aac2a
Simon Strandgaard <neoneye@gmail.com>
2024-10-07 17:11:54 +0200

    image_transition_similarity_per_row() added

--- simon_arc_lab/image_rowcol_similarity.py
+++ simon_arc_lab/image_rowcol_similarity.py
@@ -1,4 +1,7 @@
+# Measure how many rows/columns are the same in both images.
+# The images doesn't have to be the same size.
 import numpy as np
+from enum import Enum
 
 def image_transition_color_per_row(image: np.array) -> list[list[int]]:
     """
@@ -45,10 +48,43 @@ def image_transition_mass_per_row(image: np.array) -> list[list[int]]:
         mass_list_list.append(mass_list)
     return mass_list_list
 
-# def image_rowcol_similarity(image0: np.array, image1: np.array):
-#     """
-#     Measure how many rows/columns are the same in both images.
-#     The images doesn't have to be the same size.
-#     """
+def intersection_of_listlistint(items0: list[list[int]], items1: list[list[int]]) -> list[list[int]]:
+    """
+    Identify what does these lists have in common.
+    """
+    intersection_list = []
+    remaining_list0 = items0.copy()
+    for value_list1 in items1:
+        for value_list0 in remaining_list0:
+            if value_list0 == value_list1:
+                intersection_list.append(value_list0)
+                remaining_list0.remove(value_list0)
+                break
+    return intersection_list
 
-#     return []
+class TransitionType(Enum):
+    COLOR = 0
+    MASS = 1
+
+def image_transition_similarity_per_row(image0: np.array, image1: np.array, transition_type: TransitionType) -> tuple[int, int]:
+    """
+    Measure how many transitions are the same in both images.
+    The images doesn't have to be the same size.
+    """
+
+    if transition_type == TransitionType.COLOR:
+        value_list_list0 = image_transition_color_per_row(image0)
+        value_list_list1 = image_transition_color_per_row(image1)
+    elif transition_type == TransitionType.MASS:
+        value_list_list0 = image_transition_mass_per_row(image0)
+        value_list_list1 = image_transition_mass_per_row(image1)
+    else:
+        raise ValueError("Invalid transition_type")
+
+    count_union = len(value_list_list0) + len(value_list_list1)
+    if count_union == 0:
+        return (0, 0)
+
+    intersection_list = intersection_of_listlistint(value_list_list0, value_list_list1)
+    count_intersection = len(intersection_list)
+    return (count_intersection, count_union)
--- simon_arc_lab/test_image_rowcol_similarity.py
+++ simon_arc_lab/test_image_rowcol_similarity.py
@@ -43,22 +43,86 @@ class TestImageRowColSimilarity(unittest.TestCase):
         ]
         self.assertEqual(actual, expected)
 
-    # def test_20000_image_rowcol_similarity_same_size(self):
-    #     # Arrange
-    #     image0 = np.array([
-    #         [1, 1, 1, 2, 2, 2, 2],
-    #         [1, 1, 1, 2, 2, 2, 2],
-    #         [1, 1, 1, 2, 2, 2, 2],
-    #         [3, 3, 5, 5, 4, 4, 4],
-    #         [3, 3, 3, 4, 4, 4, 4]], dtype=np.uint8)
-    #     image1 = np.array([
-    #         [1, 1, 1, 2, 2, 2, 2],
-    #         [1, 1, 1, 2, 2, 2, 2],
-    #         [1, 1, 5, 5, 2, 2, 2],
-    #         [3, 3, 3, 4, 4, 4, 4],
-    #         [3, 3, 3, 4, 4, 4, 4]], dtype=np.uint8)
-    #     # Act
-    #     actual = image_rowcol_similarity(image0, image1)
-    #     print(actual)
-    #     # Assert
-    #     # self.assertEqual(actual, expected)
+    def test_20000_intersection_of_listlistint_identical(self):
+        # Arrange
+        items0 = [
+            [7],
+            [3, 4],
+            [3, 4],
+            [2, 2, 3],
+            [3, 4],
+        ]
+        # Act
+        actual = intersection_of_listlistint(items0, items0)
+        # Assert
+        expected = items0
+        self.assertEqual(actual, expected)
+
+    def test_20001_intersection_of_listlistint_some_overlap(self):
+        # Arrange
+        items0 = [
+            [7],
+            [3, 4],
+            [3, 4],
+            [2, 2, 3],
+            [3, 4],
+        ]
+        items1 = [
+            [7, 8],
+            [3, 4],
+            [3, 4],
+            [2, 2, 3],
+            [1, 2, 3, 4],
+            [3, 4, 5, 6],
+        ]
+        # Act
+        actual = intersection_of_listlistint(items0, items1)
+        # Assert
+        expected = [
+            [3, 4],
+            [3, 4],
+            [2, 2, 3],
+        ]
+        self.assertEqual(actual, expected)
+
+    def test_20002_intersection_of_listlistint_no_overlap(self):
+        # Arrange
+        items0 = [
+            [7],
+            [2, 2, 4],
+            [9, 3],
+        ]
+        items1 = [
+            [7, 8],
+            [3, 4],
+            [3, 4],
+            [2, 2, 3],
+            [1, 2, 3, 4],
+            [3, 4, 5, 6],
+        ]
+        # Act
+        actual = intersection_of_listlistint(items0, items1)
+        # Assert
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_30000_image_transition_similarity_per_row_color(self):
+        # Arrange
+        image0 = np.array([
+            [1, 1, 1, 2, 2, 2, 2],
+            [1, 1, 1, 2, 2, 2, 2],
+            [1, 1, 1, 2, 2, 2, 2],
+            [3, 3, 5, 5, 4, 4, 4],
+            [3, 3, 3, 4, 4, 4, 4]], dtype=np.uint8)
+        image1 = np.array([
+            [1, 1, 1, 2, 2, 2, 2],
+            [1, 1, 1, 2, 2, 2, 2],
+            [1, 1, 5, 5, 2, 2, 2],
+            [3, 3, 3, 4, 4, 4, 4],
+            [3, 3, 3, 4, 4, 4, 4]], dtype=np.uint8)
+        # Act
+        actual = image_transition_similarity_per_row(image0, image1, TransitionType.COLOR)
+        # print(actual)
+        # Assert
+        expected = (3, 10)
+        self.assertEqual(actual, expected)
