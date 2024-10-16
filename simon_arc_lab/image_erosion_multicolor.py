4b9e0046d2d1bbde128706993ed8b10e3a6b5803
Simon Strandgaard <neoneye@gmail.com>
2024-10-16 10:28:29 +0200

    types

--- simon_arc_lab/image_erosion_multicolor.py
+++ simon_arc_lab/image_erosion_multicolor.py
@@ -2,7 +2,7 @@ from simon_arc_lab.pixel_connectivity import *
 from simon_arc_lab.connected_component import *
 from simon_arc_lab.image_erosion import *
 
-def image_erosion_multicolor(image: np.ndarray, connectivity: PixelConnectivity) -> np.ndarray:
+def image_erosion_multicolor(image: np.array, connectivity: PixelConnectivity) -> np.array:
     component_list = ConnectedComponent.find_objects(connectivity, image)
     accumulated_mask = np.zeros_like(image)
     for component in component_list:
