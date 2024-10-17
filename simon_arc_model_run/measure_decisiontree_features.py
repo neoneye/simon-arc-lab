a4a09582f7ac77b0aebaa1b2815e264baad31ea4
Simon Strandgaard <neoneye@gmail.com>
2024-10-17 22:56:35 +0200

    nested progressbars

--- simon_arc_model_run/measure_decisiontree_features.py
+++ simon_arc_model_run/measure_decisiontree_features.py
@@ -77,21 +77,16 @@ for i in range(20):
         features = candidate_features
         break
     if features is None:
-        print(f"Failed to find a new feature set after 100 retries")
-        exit()
-    featurecomboitem = FeatureComboItem(i, features)
+        raise Exception("Failed to find a new feature set after 100 retries")
+    featurecomboitem = FeatureComboItem(i + 1, features)
     featurecomboitem_list.append(featurecomboitem)
 
 for featurecomboitem in featurecomboitem_list:
     print(f"FeatureComboItem {featurecomboitem.run_index}: {featurecomboitem.feature_names_sorted()}")
 
-exit()
-number_of_items_in_list = len(groupname_pathtotaskdir_list)
-for index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotaskdir_list):
-    save_dir = f'run_tasks_result/measure_decisiontree_features/{groupname}'
-    jsonl_filepath = f'{save_dir}/results.jsonl'
-    print(f"Processing {index+1} of {number_of_items_in_list}. Group name '{groupname}'. Results will be saved to '{save_dir}'")
 
+groupname_task_list = []
+for (groupname, path_to_task_dir) in groupname_pathtotaskdir_list:
     taskset = TaskSet.load_directory(path_to_task_dir)
 
     pending_tasks = []
@@ -101,87 +96,102 @@ for index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotaskdir_li
             pending_tasks.append(task)
         else:
             number_of_tasks_with_different_input_output_size += 1
+    
+    # truncate the list to 5 tasks
+    pending_tasks = pending_tasks[:5]
     # print(f"Number of tasks with different input/output size: {number_of_tasks_with_different_input_output_size}")
     # print(f"Number of tasks with same input/output size: {len(pending_tasks)}")
+    print(f"After filtering, number of tasks in group '{groupname}': {len(pending_tasks)}")
+    groupname_task_list.append((groupname, pending_tasks))
 
-    correct_count = 0
-    pbar = tqdm(pending_tasks, desc="Processing tasks")
-    for task in pbar:
-        pbar.set_description(f"Task {task.metadata_task_id}")
+# exit()
+pbar_combo = tqdm(featurecomboitem_list, desc="Processing combos")
+for combo in pbar_combo:
+    save_dir = f'run_tasks_result/measure_decisiontree_features/{combo.run_index}'
+    jsonl_filepath = f'{save_dir}/results.jsonl'
 
-        features = set()
+    number_of_items_in_list = len(groupname_pathtotaskdir_list)
+    pbar_group = tqdm(groupname_task_list, desc="Processing groups")
+    for (groupname, tasks) in pbar_group:
+ 
+        correct_count = 0
+        pbar_task = tqdm(pending_tasks, desc="Processing tasks")
+        for task in pbar_task:
+            pbar_task.set_description(f"{groupname} {task.metadata_task_id}")
 
-        for test_index in range(task.count_tests):
-            current_datetime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-            
-            start_time = time.perf_counter()
+            features = set()
 
-            predicted_output = DecisionTreeUtil.predict_output(
-                task, 
-                test_index, 
-                previous_prediction=None,
-                refinement_index=0, 
-                noise_level=100,
-                features=features,
-            )
+            for test_index in range(task.count_tests):
+                current_datetime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+                
+                start_time = time.perf_counter()
 
-            end_time = time.perf_counter()
-            elapsed_seconds = int(end_time - start_time)
+                predicted_output = DecisionTreeUtil.predict_output(
+                    task, 
+                    test_index, 
+                    previous_prediction=None,
+                    refinement_index=0, 
+                    noise_level=100,
+                    features=features,
+                )
 
-            input_image = task.test_input(test_index)
-            expected_output_image = task.test_output(test_index)
+                end_time = time.perf_counter()
+                elapsed_seconds = int(end_time - start_time)
 
-            is_correct = np.array_equal(predicted_output, expected_output_image)
-            if is_correct:
-                correct_count += 1
-            pbar.set_postfix({'correct': correct_count})
+                input_image = task.test_input(test_index)
+                expected_output_image = task.test_output(test_index)
 
-            count_good, count_total = image_pixel_similarity_overall(predicted_output, expected_output_image)
-            count_bad = count_total - count_good
-            if count_total > 0:
-                score = count_good * 100 // count_total
-            else:
-                score = 0
+                is_correct = np.array_equal(predicted_output, expected_output_image)
+                if is_correct:
+                    correct_count += 1
+                pbar_task.set_postfix({'correct': correct_count})
 
-            # if there was some problem, add it to the issues list
-            jsonissues = []
+                count_good, count_total = image_pixel_similarity_overall(predicted_output, expected_output_image)
+                count_bad = count_total - count_good
+                if count_total > 0:
+                    score = count_good * 100 // count_total
+                else:
+                    score = 0
 
-            # An undesired issue is when the predicted output is the same as the input image
-            is_same_as_input = np.array_equal(predicted_output, input_image)
-            if is_correct == False and is_same_as_input:
-                # pbar.write("predicted output is the same as input")
-                jsonissues.append("predicted output is the same as input")
+                # if there was some problem, add it to the issues list
+                jsonissues = []
 
-            # JSON representation of the prediction result
-            jsondata = {
-                "correct": is_correct,
-                "score": score,
-                "task_id": task.metadata_task_id,
-                "path": task.metadata_path,
-                "date": current_datetime,
-                "elapsed_seconds": elapsed_seconds,
-                "parameters": {
-                    "CONNECTED_COMPONENTS_ALL8": True,
-                    "HISTOGRAM_DIAGONAL": True,
-                    "HISTOGRAM_ROWCOL": True,
-                    "ROTATE45": True,
-                    "EROSION_ALL8": False,
-                    "LOOKAROUND_WINDOW_WIDTH": 3,
-                    "LOOKAROUND_WINDOW_HEIGHT": 1
-                },
-                "test_index": test_index,
-                "issues": jsonissues,
-                "similarity": {
-                    "score_type": "pixelwise",
-                    "count_good": count_good,
-                    "count_bad": count_bad,
-                    "count_total": count_total,
-                },
-                "input": input_image.tolist(), # Convert numpy arrays to lists
-                "expected_output": expected_output_image.tolist(), # Convert numpy arrays to lists
-                "predicted_output": predicted_output.tolist(), # Convert numpy arrays to lists
-                "version": "simon_arc_lab measure_decisiontree_features 2024-oct-17"
-            }
+                # An undesired issue is when the predicted output is the same as the input image
+                is_same_as_input = np.array_equal(predicted_output, input_image)
+                if is_correct == False and is_same_as_input:
+                    # pbar.write("predicted output is the same as input")
+                    jsonissues.append("predicted output is the same as input")
 
-            # Save the result to a jsonl file
-            append_to_jsonl_file(jsonl_filepath, jsondata)
+                # JSON representation of the prediction result
+                jsondata = {
+                    "correct": is_correct,
+                    "score": score,
+                    "task_id": task.metadata_task_id,
+                    "path": task.metadata_path,
+                    "date": current_datetime,
+                    "elapsed_seconds": elapsed_seconds,
+                    "parameters": {
+                        "CONNECTED_COMPONENTS_ALL8": True,
+                        "HISTOGRAM_DIAGONAL": True,
+                        "HISTOGRAM_ROWCOL": True,
+                        "ROTATE45": True,
+                        "EROSION_ALL8": False,
+                        "LOOKAROUND_WINDOW_WIDTH": 3,
+                        "LOOKAROUND_WINDOW_HEIGHT": 1
+                    },
+                    "test_index": test_index,
+                    "issues": jsonissues,
+                    "similarity": {
+                        "score_type": "pixelwise",
+                        "count_good": count_good,
+                        "count_bad": count_bad,
+                        "count_total": count_total,
+                    },
+                    "input": input_image.tolist(), # Convert numpy arrays to lists
+                    "expected_output": expected_output_image.tolist(), # Convert numpy arrays to lists
+                    "predicted_output": predicted_output.tolist(), # Convert numpy arrays to lists
+                    "version": "simon_arc_lab measure_decisiontree_features 2024-oct-17"
+                }
+
+                # Save the result to a jsonl file
+                append_to_jsonl_file(jsonl_filepath, jsondata)
