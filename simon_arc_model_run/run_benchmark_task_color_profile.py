61f4670bb656f06304f06a1993a2766e0f24744d
Simon Strandgaard <neoneye@gmail.com>
2024-10-27 12:12:58 +0100

    Only run it on the testdata ARC-AGI dataset

--- simon_arc_model_run/run_benchmark_task_color_profile.py
+++ simon_arc_model_run/run_benchmark_task_color_profile.py
@@ -14,14 +14,14 @@ if not os.path.isdir(path_to_arc_dataset_collection_dataset):
     sys.exit(1)
 
 groupname_pathtotaskdir_list = [
-    ('arcagi_training', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/training')),
-    ('arcagi_evaluation', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/evaluation')),
+    # ('arcagi_training', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/training')),
+    # ('arcagi_evaluation', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/evaluation')),
     # ('tama', os.path.join(path_to_arc_dataset_collection_dataset, 'arc-dataset-tama/data')),
     # ('diva', os.path.join(path_to_arc_dataset_collection_dataset, 'arc-dataset-diva/data')),
     # ('miniarc', os.path.join(path_to_arc_dataset_collection_dataset, 'Mini-ARC/data')),
     # ('conceptarc', os.path.join(path_to_arc_dataset_collection_dataset, 'ConceptARC/data')),
-    ('rearc_easy', os.path.join(path_to_arc_dataset_collection_dataset, 'RE-ARC/data/easy')),
-    ('rearc_hard', os.path.join(path_to_arc_dataset_collection_dataset, 'RE-ARC/data/hard')),
+    # ('rearc_easy', os.path.join(path_to_arc_dataset_collection_dataset, 'RE-ARC/data/easy')),
+    # ('rearc_hard', os.path.join(path_to_arc_dataset_collection_dataset, 'RE-ARC/data/hard')),
     # ('sortofarc', os.path.join(path_to_arc_dataset_collection_dataset, 'Sort-of-ARC/data')),
     # ('synth_riddles', os.path.join(path_to_arc_dataset_collection_dataset, 'synth_riddles/data')),
     # ('Sequence_ARC', os.path.join(path_to_arc_dataset_collection_dataset, 'Sequence_ARC/data')),
@@ -30,12 +30,12 @@ groupname_pathtotaskdir_list = [
     # ('dbigham', os.path.join(path_to_arc_dataset_collection_dataset, 'dbigham/data')),
     # ('arc-community', os.path.join(path_to_arc_dataset_collection_dataset, 'arc-community/data')),
     # ('IPARC', os.path.join(path_to_arc_dataset_collection_dataset, 'IPARC/data')),
-    # ('testdata', os.path.join(PROJECT_ROOT, 'testdata', 'ARC-AGI/data')),
+    ('testdata', os.path.join(PROJECT_ROOT, 'testdata', 'ARC-AGI/data')),
 ]
 
 invalid_task_id_list = [
     # ARC-AGI
-    'a8610ef7',
+    # 'a8610ef7',
     # IPARC
     'CatB_Hard_Task005',
     'CatB_Hard_Task017',
@@ -48,7 +48,7 @@ for groupname, path_to_task_dir in groupname_pathtotaskdir_list:
         print(f"path_to_task_dir directory '{path_to_task_dir}' does not exist.")
         sys.exit(1)
 
-benchmark = BenchmarkTaskColorProfile()
+benchmark = BenchmarkTaskColorProfile(verbose=False)
 number_of_items_in_list = len(groupname_pathtotaskdir_list)
 total_elapsed_time = 0
 for index, (groupname, path_to_task_dir) in enumerate(groupname_pathtotaskdir_list):
