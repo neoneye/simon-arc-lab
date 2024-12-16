3b79f4e0089604da9940c39fc46532a9e5760e22
Simon Strandgaard <neoneye@gmail.com>
2024-12-16 17:27:39 +0100

    prompt tweaking

--- simon_llamaindex/run_task_openai.py
+++ simon_llamaindex/run_task_openai.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import sys
 import os
+from tqdm import tqdm
 import numpy as np
 from dotenv import dotenv_values
 from llama_index.llms.openai import OpenAI
@@ -109,10 +110,16 @@ def serialize_image(image: np.array) -> str:
 def create_prompt_for_task(task: Task, test_index: int) -> str:
     tcp = TaskColorProfile(task)
     color_profile_prediction = tcp.predict_output_colors_for_test_index(test_index)
-    print(f"color_profile_prediction: {color_profile_prediction}")
+
     items = []
     items.append("# Puzzle solving")
-    items.append("what ARC (Abstraction & Reasoning Corpus) transformation happens here?")
+    items.append("You are an expert in ARC (Abstraction & Reasoning Corpus).")
+    items.append("")
+    items.append("You are also an LLM so tokenization may impact how you see the image.") 
+    items.append("for example three zeros may be just a single token, or it may be multiple tokens.")
+    items.append("for example the color 'blue' may be just a single token, or it may be multiple tokens.")
+    items.append("You cannot assume that the image you perceive is correct due to the tokenization.")
+    items.append("For this reason multiple representations are provided, so you can better reason about the puzzle.")
     items.append("")
     for example_index in range(task.count_examples):
         if example_index > 0:
@@ -127,27 +134,36 @@ def create_prompt_for_task(task: Task, test_index: int) -> str:
         items.append(f'## Transformation {example_index} Output')
         items.append(serialize_image(output_image))
 
-    if True:
-        items.append("")
-        items.append("")
-        input_image = task.test_input(test_index)
-        items.append(f'## Transformation Test Input')
-        items.append(serialize_image(input_image))
-        items.append("")
-        items.append("")
-        items.append(f'## Transformation Test Output')
-        items.append("")
-        items.append("This is what the model should predict. Before giving the answer, please reflect on what transformation happens.")
-        items.append("")
-        for y, (certain, color_set) in enumerate(color_profile_prediction.certain_colorset_list):
-            color_list = list(color_set)
-            color_list.sort()
-            color_list_str = format_color_list(color_list)
-            items.append(f"- color that are likely to be in the output: {color_list_str}")
+    items.append("")
+    items.append("")
+    input_image = task.test_input(test_index)
+    items.append(f'## Transformation Test Input')
+    items.append(serialize_image(input_image))
+    items.append("")
+    items.append("")
+    items.append(f'## Transformation Test Output')
+    items.append("")
+    items.append("This is what the model should predict. Before giving the answer, please reflect on what transformation happens.")
+    items.append("")
+    items.append("My own thoughts about the output:")
+    for y, (certain, color_set) in enumerate(color_profile_prediction.certain_colorset_list):
+        color_list = list(color_set)
+        color_list.sort()
+        color_list_str = format_color_list(color_list)
+        items.append(f"- I guess these colors are likely present in the output: {color_list_str}")
 
+    items.append("")
+    items.append("# Task A - What transformation happens?")
+    items.append("")
+    items.append("# Task B - Predict the output")
+    items.append("")
+    items.append("Use emoji representation for the output.")
+    items.append("")
+    items.append("# Task C - double check your own answer")
+    items.append("")
 
     result = "\n".join(items)
-    print(f"bytes: {len(result)}")
+    # print(f"bytes: {len(result)}")
     return result
 
 number_of_items_in_list = len(datasetid_groupname_pathtotaskdir_list)
@@ -165,9 +181,10 @@ for index, (dataset_id, groupname, path_to_task_dir) in enumerate(datasetid_grou
 
     print(f"Number of tasks for processing: {len(taskset.tasks)}")
 
-    for task in taskset.tasks:
+    pbar = tqdm(taskset.tasks, desc=f"Processing tasks in {groupname}", dynamic_ncols=True)
+    for task in pbar:
         task_id = task.metadata_task_id
-        print(f"Task id: {task_id}")
+        pbar.set_postfix_str(f"Task: {task_id}")
 
         for test_index in range(task.count_tests):
             prompt = create_prompt_for_task(task, test_index)
