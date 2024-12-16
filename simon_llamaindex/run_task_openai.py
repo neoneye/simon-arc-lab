f1c346bd03c21eb57a19ee26dc1669c235ff0824
Simon Strandgaard <neoneye@gmail.com>
2024-12-16 11:03:53 +0100

    load a task

---
+++ simon_llamaindex/run_task_openai.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+import sys
+import os
+from dotenv import dotenv_values
+from llama_index.llms.openai import OpenAI
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, PROJECT_ROOT)
+
+from simon_arc_lab.task import Task
+from simon_arc_lab.taskset import TaskSet
+
+dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '.env'))
+dotenv_dict = dotenv_values(dotenv_path=dotenv_path)
+
+run_id = datetime.now().strftime("%Y%m%d_%H%M%S")
+print(f"Run id: {run_id}")
+
+path_to_arc_dataset_collection_dataset = '/Users/neoneye/git/arc-dataset-collection/dataset'
+if not os.path.isdir(path_to_arc_dataset_collection_dataset):
+    print(f"ARC dataset collection directory '{path_to_arc_dataset_collection_dataset}' does not exist.")
+    sys.exit(1)
+
+datasetid_groupname_pathtotaskdir_list = [
+    ('ARC-AGI', 'arcagi', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data')),
+    # ('ARC-AGI', 'arcagi_training', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/training')),
+    # ('ARC-AGI', 'arcagi_evaluation', os.path.join(path_to_arc_dataset_collection_dataset, 'ARC/data/evaluation')),
+    # ('arc-dataset-tama', 'tama', os.path.join(path_to_arc_dataset_collection_dataset, 'arc-dataset-tama/data')),
+    # ('Mini-ARC', 'miniarc', os.path.join(path_to_arc_dataset_collection_dataset, 'Mini-ARC/data')),
+    # ('ConceptARC', 'conceptarc', os.path.join(path_to_arc_dataset_collection_dataset, 'ConceptARC/data')),
+    # ('ARC-AGI', 'testdata', os.path.join(PROJECT_ROOT, 'testdata', 'ARC-AGI/data')),
+]
+
+for dataset_id, groupname, path_to_task_dir in datasetid_groupname_pathtotaskdir_list:
+    if not os.path.isdir(path_to_task_dir):
+        print(f"path_to_task_dir directory '{path_to_task_dir}' does not exist.")
+        sys.exit(1)
+
+task_ids_of_interest = [
+    '0b17323b'
+]
+
+number_of_items_in_list = len(datasetid_groupname_pathtotaskdir_list)
+for index, (dataset_id, groupname, path_to_task_dir) in enumerate(datasetid_groupname_pathtotaskdir_list):
+    save_dir = f'run_tasks_result/{run_id}/{groupname}'
+    print(f"Processing {index+1} of {number_of_items_in_list}. Group name '{groupname}'. Results will be saved to '{save_dir}'")
+
+    taskset = TaskSet.load_directory(path_to_task_dir)
+    taskset.keep_tasks_with_id(set(task_ids_of_interest), verbose=False)
+
+    if len(taskset.tasks) == 0:
+        print(f"Skipping group: {groupname}, due to no tasks to process.")
+        continue
+
+    print(f"Number of tasks for processing: {len(taskset.tasks)}")
+
+
+exit()
+
+llm = OpenAI(
+    model="gpt-4o-mini",
+    api_key=dotenv_dict['OPENAI_API_KEY'],
+)
+
+from llama_index.core.llms import ChatMessage
+
+messages = [
+    ChatMessage(
+        role="system", content="You are a pirate with a colorful personality"
+    ),
+    ChatMessage(role="user", content="What is your name"),
+]
+resp = llm.stream_chat(messages)
+
+for r in resp:
+    print(r.delta, end="")
