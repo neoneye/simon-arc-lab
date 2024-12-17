725ca2e73abd7aaad0f3c3da90f784f539f554b1
Simon Strandgaard <neoneye@gmail.com>
2024-12-17 14:51:45 +0100

    testing edge cases

--- simon_arc_lab/test_image_sparse_representation.py
+++ simon_arc_lab/test_image_sparse_representation.py
@@ -32,9 +32,8 @@ class TestImageSparseRepresentation(unittest.TestCase):
         actual_image, actual_status = dictionary_to_image(input_str)
         expected = np.array([
             [7, 7, 9],
-            [8, 7, 9],
-        ], dtype=np.uint8)
-        self.assertTrue(np.array_equal(actual_image, expected))
+            [8, 7, 9]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
         self.assertIsNone(actual_status)
 
     def test_20001_dictionary_to_image_out_of_order(self):
@@ -42,9 +41,8 @@ class TestImageSparseRepresentation(unittest.TestCase):
         actual_image, actual_status = dictionary_to_image(input_str)
         expected = np.array([
             [7, 7, 9],
-            [8, 7, 9],
-        ], dtype=np.uint8)
-        self.assertTrue(np.array_equal(actual_image, expected))
+            [8, 7, 9]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
         self.assertIsNone(actual_status)
 
     def test_20002_dictionary_to_image_with_newlines(self):
@@ -52,9 +50,8 @@ class TestImageSparseRepresentation(unittest.TestCase):
         actual_image, actual_status = dictionary_to_image(input_str)
         expected = np.array([
             [7, 7, 9],
-            [8, 7, 9],
-        ], dtype=np.uint8)
-        self.assertTrue(np.array_equal(actual_image, expected))
+            [8, 7, 9]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
         self.assertIsNone(actual_status)
 
     def test_20003_dictionary_to_image_with_spaces(self):
@@ -62,7 +59,65 @@ class TestImageSparseRepresentation(unittest.TestCase):
         actual_image, actual_status = dictionary_to_image(input_str)
         expected = np.array([
             [7, 7, 9],
-            [8, 7, 9],
-        ], dtype=np.uint8)
-        self.assertTrue(np.array_equal(actual_image, expected))
+            [8, 7, 9]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
         self.assertIsNone(actual_status)
+
+    def test_20004_dictionary_to_image_with_background_a(self):
+        input_str = "{'width':2,'height':4,'background':42}"
+        actual_image, actual_status = dictionary_to_image(input_str)
+        expected = np.array([
+            [42, 42],
+            [42, 42],
+            [42, 42],
+            [42, 42]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
+        self.assertIsNone(actual_status)
+
+    def test_20005_dictionary_to_image_with_background_b(self):
+        input_str = "{'width':3,'height':3,'background':0, (1,1):  99}"
+        actual_image, actual_status = dictionary_to_image(input_str)
+        expected = np.array([
+            [0, 0, 0],
+            [0, 99, 0],
+            [0, 0, 0]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
+        self.assertIsNone(actual_status)
+
+    def test_20100_dictionary_to_image_problem_pixel_outside(self):
+        input_str = "{'width':3,'height':3,'background':0, (100,5):  99}"
+        actual_image, actual_status = dictionary_to_image(input_str)
+        expected = np.array([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
+        self.assertEqual(actual_status, "1 pixels outside")
+
+    def test_20101_dictionary_to_image_problem_unassigned(self):
+        input_str = "{'width':1,'height':1}"
+        actual_image, actual_status = dictionary_to_image(input_str)
+        expected = np.array([
+            [255]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
+        self.assertEqual(actual_status, "1 unassigned pixels")
+
+    def test_20102_dictionary_to_image_problem_unassigned_and_outside(self):
+        input_str = "{'width':1,'height':1,(100,5):99}}"
+        actual_image, actual_status = dictionary_to_image(input_str)
+        expected = np.array([
+            [255]], dtype=np.uint8)
+        np.testing.assert_array_equal(actual_image, expected)
+        self.assertEqual(actual_status, "1 pixels outside, 1 unassigned pixels")
+
+    def test_20200_dictionary_to_image_width_is_missing(self):
+        input_str = "{'wi_typo_dth':1,'height':1,(0,0):42}}"
+        with self.assertRaises(ValueError) as context:
+            dictionary_to_image(input_str)
+        self.assertEqual(str(context.exception), "Missing 'width'")
+
+    def test_20201_dictionary_to_image_height_is_missing(self):
+        input_str = "{'width':1,'hei_typo_ght':1,(0,0):42}}"
+        with self.assertRaises(ValueError) as context:
+            dictionary_to_image(input_str)
+        self.assertEqual(str(context.exception), "Missing 'height'")
