f13bf9de4a7158ddc97923cf148bedb85f6c1afd
Simon Strandgaard <neoneye@gmail.com>
2025-01-07 13:51:40 +0100

    Dividing by 2 makes no difference

--- simon_arc_model/data_from_image_builder.py
+++ simon_arc_model/data_from_image_builder.py
@@ -538,8 +538,7 @@ class DataFromImageBuilder:
 
     def make_lonely_pixels(self):
         image_with_lonely_pixels = find_lonely_pixels(self.image)
-        normalized_image = image_with_lonely_pixels * 0.5
-        self.data['lonely_pixels'] = normalized_image.flatten().tolist()
+        self.data['lonely_pixels'] = image_with_lonely_pixels.flatten().tolist()
 
     def make_count_same_color_as_center(self, lookaround_size: int):
         n = lookaround_size
