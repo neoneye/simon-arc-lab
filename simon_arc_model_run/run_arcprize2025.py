eb8423a30d9fded6934d96bab35b2d7c616f6878
Simon Strandgaard <neoneye@gmail.com>
2025-10-13 18:22:09 +0200

    run1() now takes a few more parameters, so the path to the submission.json file can be provided

--- simon_arc_model_run/run_arcprize2025.py
+++ simon_arc_model_run/run_arcprize2025.py
@@ -13,18 +13,19 @@ from simon_arc_lab.taskset import TaskSet
 from simon_arc_lab.gallery_generator import gallery_generator_run
 from simon_arc_model.work_manager_decision_tree import WorkManagerDecisionTree
 
-def run1(input_challenges_path: str, production: bool=False):
+def create_run_id() -> str:
+    return datetime.now().strftime("%Y%m%d_%H%M%S")
+
+def run1(production: bool, run_id: str, input_challenges_path: str, save_debug_dir: str, output_submission_path: str, gallery_title: str):
+    # print the parameters
     mode_name = 'production' if production else 'developer'
     print(f"Mode: {mode_name}")
-
-    run_id = datetime.now().strftime("%Y%m%d_%H%M%S")
     print(f"Run id: {run_id}")
+    print(f"Debug data will be saved to '{save_debug_dir}'")
+    print(f"Submission will be saved to '{output_submission_path}'")
+    print(f"Gallery will be saved to '{save_debug_dir}'")
 
     dataset_id = 'ARC-AGI-2-test'
-    groupname = 'arcprize2025-test'
-
-    save_dir = f'run_tasks_result/{run_id}/{groupname}'
-    print(f"Group name '{groupname}'. Results will be saved to '{save_dir}'")
 
     taskset = TaskSet.load_kaggle_arcprize2024_json(input_challenges_path)
     if not production:
@@ -33,18 +34,23 @@ def run1(input_challenges_path: str, production: bool=False):
     wm = WorkManagerDecisionTree(run_id=run_id, dataset_id=dataset_id, taskset=taskset, cache_dir=None, incorrect_predictions_jsonl_path=None)
     # wm.truncate_work_items(20)
     # wm.process_all_work_items()
-    wm.process_all_work_items(save_dir=save_dir)
+    wm.process_all_work_items(save_dir=save_debug_dir)
     # wm.process_all_work_items(show=True)
     wm.discard_items_where_predicted_output_is_identical_to_the_input()
     wm.summary()
 
-    gallery_title = f'{groupname}, {run_id}'
-    gallery_generator_run(save_dir, title=gallery_title)
+    gallery_generator_run(save_debug_dir, title=gallery_title)
 
-    # wm.save_arcprize2024_submission_file('submission.json')
+    # Save the submission
+    wm.save_arcprize2024_submission_file(output_submission_path)
 
 if __name__ == '__main__':
+    run_id: str = create_run_id()
     run1(
+        production=False,
+        run_id=run_id,
         input_challenges_path='testdata/kaggle-arc-prize-2025/arc-agi_test_challenges.json', 
-        production=False
+        save_debug_dir=f'run_tasks_result/{run_id}',
+        output_submission_path=f'run_tasks_result/{run_id}/submission.json',
+        gallery_title=f"arcprize2025-test, {run_id}"
     )
